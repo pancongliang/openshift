456185f6c731988d9835456263a9d734a0a1e0c8
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-25 18:48:41 +0800

    Update 7_manually_install_on_each_nod.sh

--- ocp_install/7_manually_install_on_each_nod.sh
+++ ocp_install/7_manually_install_on_each_nod.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cat << EOF > 7_manually_install_on_each_nod.sh
-curl http://$BASTION_IP:8080/bootstrap-set-ip-1.sh | bash
-curl http://$BASTION_IP:8080/bootstrap-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/bootstrap-set-ip-1.sh | bash
+curl http://$BASTION_IP:8080/pre/bootstrap-installer-2.sh | bash
 
-curl http://$BASTION_IP:8080/master01-set-ip-1.sh | bash
-curl http://$BASTION_IP:8080/master01-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/master01-set-ip-1.sh | bash
+curl http://$BASTION_IP:8080/pre/master01-installer-2.sh | bash
 
-curl http://$BASTION_IP:8080/master02-set-ip-1.sh | bash
-curl http://$BASTION_IP:8080/master02-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/master02-set-ip-1.sh | bash
+curl http://$BASTION_IP:8080/pre/master02-installer-2.sh | bash
 
-curl http://$BASTION_IP:8080/master03-set-ip-1.sh | bash
-curl http://$BASTION_IP:8080/master03-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/master03-set-ip-1.sh | bash
+curl http://$BASTION_IP:8080/pre/master03-installer-2.sh | bash
 
-curl http://$BASTION_IP:8080/worker01-installer-2.sh | bash
-curl http://$BASTION_IP:8080/worker01-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/worker01-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/worker01-installer-2.sh | bash
 
-curl http://$BASTION_IP:8080/worker02-set-ip-1.sh | bash
-curl http://$BASTION_IP:8080/worker02-installer-2.sh | bash
+curl http://$BASTION_IP:8080/pre/worker02-set-ip-1.sh | bash
+curl http://$BASTION_IP:8080/pre/worker02-installer-2.sh | bash
 EOF
 
 ls $INSTALL_DIR/
