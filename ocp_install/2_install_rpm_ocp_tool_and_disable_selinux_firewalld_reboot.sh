df69adde18e29e8c3564544ff3daba3791d843e9
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-25 16:13:04 +0800

    Create 2_install_rpm_ocp_tool_and_disable_selinux_firewalld_reboot.sh

---
+++ ocp_install/2_install_rpm_ocp_tool_and_disable_selinux_firewalld_reboot.sh
@@ -0,0 +1,32 @@
+# Install required packages
+yum install -y wget net-tools podman bind-utils bind haproxy git bash-completion vim jq nfs-utils httpd httpd-tools skopeo httpd-manual
+
+# openshift-install:
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-install-linux.tar.gz
+tar xvf openshift-install-linux.tar.gz
+mv openshift-install /usr/local/bin/
+
+# oc CLI tools:
+curl https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz --output openshift-client-linux.tar.gz
+tar xvf openshift-client-linux.tar.gz
+mv oc kubectl /usr/local/bin/
+
+# oc-mirror tools:
+curl -O https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/oc-mirror.tar.gz
+tar -xvf oc-mirror.tar.gz
+chmod +x oc-mirror && mv oc-mirror /usr/local/bin/
+
+# butane tools:
+curl https://mirror.openshift.com/pub/openshift-v4/clients/butane/latest/butane --output butane
+chmod a+x butane && mv butane /usr/local/bin/
+
+ls /usr/local/bin/
+
+# Disable firewalld
+systemctl disable firewalld
+
+# Disable SELinux
+sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config
+
+# Reboot the system
+reboot
