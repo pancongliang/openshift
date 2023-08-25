5d2c087bb6808ce3ec8388e798e007f75c6ddbf2
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-25 16:30:38 +0800

    Create 4_download_image.sh

---
+++ ocp_install/4_download_image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+oc adm -a ${LOCAL_SECRET_JSON} release mirror \
+  --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} \
+  --to=${LREGISTRY_HOSTNAM}:5000/${LOCAL_REPOSITORY} \
+  --to-release-image=${LREGISTRY_HOSTNAM}:5000/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 
