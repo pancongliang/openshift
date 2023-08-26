207d6cd926ce584e66cd1c14bf909720ef34b339
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-26 13:56:51 +0800

    Update 4_download_ocp_image.sh

--- ocp_install/4_download_ocp_image.sh
+++ ocp_install/4_download_ocp_image.sh
@@ -5,6 +5,6 @@ oc adm -a ${LOCAL_SECRET_JSON} release mirror \
   --to=${REGISTRY_HOSTNAME}:5000/${LOCAL_REPOSITORY} \
   --to-release-image=${REGISTRY_HOSTNAME}:5000/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 
 
-sudo sleep 60
+sudo sleep 10
 
 curl -u $REGISTRY_ID:$REGISTRY_PW -k https://${REGISTRY_HOSTNAME}:5000/v2/_catalog
