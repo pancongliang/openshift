845c36c486c9740552046b2d7a7eb747f595f3c7
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-26 14:13:56 +0800

    Update 5_create_bootstrap_master_worker_ip_and_coreos_install_file.sh

--- ocp_install/5_create_bootstrap_master_worker_ip_and_coreos_install_file.sh
+++ ocp_install/5_create_bootstrap_master_worker_ip_and_coreos_install_file.sh
@@ -1,69 +1,69 @@
 #!/bin/bash
-cat << EOF > $INSTALL_DIR/bootstrap-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/bootstrap-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $BOOTSTRAP_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 sudo sleep 10
 EOF
 
-cat << EOF > $INSTALL_DIR/master01-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/master01-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $MASTER01_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 EOF
 
-cat << EOF > $INSTALL_DIR/master02-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/master02-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $MASTER02_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 EOF
 
-cat << EOF > $INSTALL_DIR/master03-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/master03-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $MASTER03_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 EOF
 
-cat << EOF > $INSTALL_DIR/master02-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/master02-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $WORKER01_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 EOF
 
-cat << EOF > $INSTALL_DIR/worker02-set-ip-1.sh
+cat << EOF > $OCP_INSTALL_DIR/worker02-set-ip-1.sh
 nmcli con mod $NODE_NETWORK_WIRED_CONNECTION ipv4.addresses $WORKER02_IP/$NETMASK ipv4.gateway $GATEWAY_IP ipv4.dns $DNS_IP ipv4.method manual connection.autoconnect yes
 sudo systemctl restart NetworkManager
 EOF
 
-cat << EOF > $INSTALL_DIR/bootstrap-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/bootstrap-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/bootstrapbk.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-cat << EOF > $INSTALL_DIR/master01-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/master01-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/master01.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-cat << EOF > $INSTALL_DIR/master02-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/master02-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/master02.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-cat << EOF > $INSTALL_DIR/master03-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/master03-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/master03.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-cat << EOF > $INSTALL_DIR/worker01-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/worker01-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/worker01.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-cat << EOF > $INSTALL_DIR/worker02-installer-2.sh
+cat << EOF > $OCP_INSTALL_DIR/worker02-installer-2.sh
 sudo coreos-installer install --copy-network \
      --ignition-url=http://$BASTION_IP:8080/pre/worker02.ign /dev/$NODE_DISK_PARTITION \
      --insecure-ignition
 EOF
 
-ls $INSTALL_DIR/
+ls $OCP_INSTALL_DIR/
