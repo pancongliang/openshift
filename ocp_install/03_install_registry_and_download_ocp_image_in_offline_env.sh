fa7e78f66bd2d3fa4492492872baf2ae389b1c10
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-28 14:36:10 +0800

    Update 03_install_registry_and_download_ocp_image_in_offline_env.sh

--- ocp_install/03_install_registry_and_download_ocp_image_in_offline_env.sh
+++ ocp_install/03_install_registry_and_download_ocp_image_in_offline_env.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 #######################################################
+
 echo ====== Check if there is an existing certificate ======
 # Define the certificate file paths
 certificate_paths=(
@@ -207,17 +209,8 @@ done
 #######################################################
 
 
-echo ====== Registry login authentication file ======
+echo ====== Login to the registry ======
 # Login to the registry
-podman login -u "$REGISTRY_ID" -p "$REGISTRY_PW" --authfile "${PULL_SECRET}" "${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:5000"
-
-# Check the return code of the podman login command
-if [ $? -eq 0 ]; then
-    echo "Successfully logged in to the registry."
-else
-    echo "Failed to log in to the registry."
-fi
-
 
 podman login -u "$REGISTRY_ID" -p "$REGISTRY_PW"  "${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:5000"
 
@@ -230,21 +223,3 @@ fi
 
 
 #######################################################
-
-
-echo ====== Download ocp images ======
-# Execute oc adm release mirror command
-oc adm -a ${{PULL_SECRET}} release mirror \
-  --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} \
-  --to=${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:5000/${LOCAL_REPOSITORY} \
-  --to-release-image=${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:5000/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 
-  
-# Check the return code of the oc adm release mirror command
-if [ $? -eq 0 ]; then
-    echo "Successfully mirrored release."
-else
-    echo "Failed to mirror release."
-fi
-sudo sleep 10
-
-curl -u ${REGISTRY_ID}:${REGISTRY_PW} -k https://${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:5000/v2/_catalog
