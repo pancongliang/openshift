31ba6542ed6af558ddae769189ead6151d8e7258
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-29 18:59:40 +0800

    Update 04_mirror_the_ocp_image_to_the_registry.sh

--- ocp_install/04_mirror_the_ocp_image_to_the_registry.sh
+++ ocp_install/04_mirror_the_ocp_image_to_the_registry.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-#######################################################
+
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=45  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
+
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
+}
+
+######
+
+# Task: Install infrastructure rpm
+PRINT_TASK "[TASK: Mirror ocp image to mirror-registry]"
 
 echo ====== Registry login authentication file ======
 # Login to the registry
