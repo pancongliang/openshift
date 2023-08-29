1cd547f4d414ed692dc3f6f96a40bb3c706f0143
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-08-29 22:42:47 +0800

    Update 00_disable_firewall_and_selinux.sh

--- ocp_install/00_disable_firewall_and_selinux.sh
+++ ocp_install/00_disable_firewall_and_selinux.sh
@@ -1,57 +1,61 @@
 #!/bin/bash
-#######################################################
 
-echo ====== Disable and check firewalld ======
-# disable firewalld
-systemctl disable firewalld
-systemctl stop firewalld
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=90  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
 
-# Wait for a short moment for httpd to start
-sleep 10
-
-# Check if a service is disabled
-check_service_disabled() {
-    service_name=$1
-    if systemctl is-enabled "$service_name" | grep -q "disabled"; then
-        return 0
-    else
-        return 1
-    fi
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
 }
 
-# Check if a service is stopped
-check_service_stopped() {
-    service_name=$1
-    if systemctl is-active "$service_name" | grep -q "inactive"; then
-        return 0
+######
+
+# Task: Disable and stop firewalld service
+PRINT_TASK "[TASK: Disable and stop firewalld service]"
+
+# List of services to handle
+services=("firewalld")
+
+# Loop through each service in the list
+for service in "${services[@]}"; do
+    # Disable the service
+    systemctl disable "$service" &>/dev/null
+    disable_status=$?
+    # Stop the service
+    systemctl stop "$service" &>/dev/null
+    stop_status=$?
+    # Check if both disable and stop commands executed successfully
+    if [ $disable_status -eq 0 ] && [ $stop_status -eq 0 ]; then
+        echo "ok: [$service service stopped and disabled]"
     else
-        return 1
+        echo "failed: [$service service is not stopped or disabled]"
     fi
-}
+done
 
-# Display status message
-display_status_message() {
-    service_name=$1
-    if check_service_disabled "$service_name" && check_service_stopped "$service_name"; then
-        echo "$service_name service is successfully disabled and stopped."
-    elif ! check_service_disabled "$service_name"; then
-        echo "Error: $service_name service is not disabled."
-    elif ! check_service_stopped "$service_name"; then
-        echo "Error: $service_name service is not stopped."
-    else
-        echo "Error: Unable to determine status of $service_name service."
-    fi
-}
+######
 
-# Check and display status for specific services
-display_status_message "firewalld"
+# Task: Change SELinux security policy
+PRINT_TASK "[TASK: Change SELinux security policy]"
 
-#######################################################
+# Temporarily set SELinux security policy to permissive
+setenforce 0 &>/dev/null
+# Check temporary SELinux security policy
+temporary_status=$(getenforce)
+# Check if temporary SELinux security policy is permissive or disabled
+if [[ $temporary_status == "Permissive" || $temporary_status == "Disabled" ]]; then
+    echo "OK: [selinux temporary security policy is $temporary_status]"
+else
+    echo "Failed: [selinux temporary security policy is $temporary_status (expected Permissive or Disabled)]"
+fi
 
-echo ====== Disable and check SeLinux ======
-# Get current SELinux status
-SELINUX=$(getenforce)
-echo "$SELINUX"
+
+
+检查/etc/selinux/config中的selinux permanent security policy
+如果是Enforcing则更改为permissive，更改完成后提示"OK: [selinux permanent security policy is $permanent_status]"
+如果是disabled 则无需更改直接提示"OK: [selinux permanent security policy is $permanent_status]"
+如果更改失败则提示"Failed: [selinux permanent security policy is $permanent_status (expected Permissive or Disabled)]"
 
 # Check if SELinux is enforcing and update config if needed
 if [ "$SELINUX" = "Enforcing" ]; then
@@ -62,15 +66,15 @@ fi
 setenforce 0 &>/dev/null
 
 # Check current SELinux status (temporary)
-current_status=$(getenforce)
-echo "Current SELinux status (temporary): $current_status"
+temporary_status=$(getenforce)
+echo "Current SELinux status (temporary): $temporary_status"
 
 # Check permanent SELinux status
 config_file_status=$(grep -E "^SELINUX=" /etc/selinux/config | awk -F= '{print $2}')
 echo "Permanent SELinux status: $config_file_status"
 
 # Check if both temporary and permanent statuses are not permissive or disabled
-if [ "$current_status" != "Permissive" -a "$current_status" != "Disabled" ] || \
+if [ "$temporary_status" != "Permissive" -a "$temporary_status" != "Disabled" ] || \
    [ "$config_file_status" != "permissive" -a "$config_file_status" != "disabled" ]; then
     echo "Error: SELinux should be set to 'permissive' or 'disabled'."
 fi
