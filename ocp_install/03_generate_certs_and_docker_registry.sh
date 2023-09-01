207ac0353aea78fb4b539aa10682d68b10a5d86f
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-09-01 19:07:56 +0800

    Update 03_generate_certs_and_docker_registry.sh

--- ocp_install/03_generate_certs_and_docker_registry.sh
+++ ocp_install/03_generate_certs_and_docker_registry.sh
@@ -13,25 +13,49 @@ PRINT_TASK() {
 
 
 # === Task: Prompt for required variables ===
-export REGISTRY_DOMAIN="docker.registry.example.com"
-export USER="admin"
-export PASSWD="passwd"                      # 6 characters or more
-export REGISTRY_INSTALL_PATH="/var/mirror-registry"
+export REGISTRY_DOMAIN="mirror.registry.example.com"
+export USER="root"
+export PASSWD="password"                      # 8 characters or more
+export REGISTRY_INSTALL_PATH="/var/registry"
 
+# === Task: Delete existing duplicate data ===
+PRINT_TASK "[TASK: Delete existing duplicate data]"
 
-# Delete existing registry/file
-${REGISTRY_INSTALL_PATH}/mirror-registry uninstall -v \
-   --autoApprove --ssh-key ~/.ssh/id_rsa.pub \
-   --quayRoot ${REGISTRY_INSTALL_PATH}
+# Check if there is an active mirror registry pod
+if podman pod ps | grep -P '(?=.*\bquay-pod\b)(?=.*\bRunning\b)(?=.*\b4\b)' >/dev/null; then
+    # If the mirror registry pod is running, uninstall it
+    ${REGISTRY_INSTALL_PATH}/mirror-registry uninstall --autoApprove --quayRoot ${REGISTRY_INSTALL_PATH}    
+    # Check the exit status of the uninstall command
+    if [ $? -eq 0 ]; then
+        echo "ok: [uninstall the mirror registry]"
+    else
+        echo "failed: [uninstall the mirror registry]"
+    fi
+else
+    echo "skipping: [no active mirror registry pod found. skipping uninstallation]"
+fi
 
-rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.ca.crt &>/dev/null
-rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.crt &>/dev/null
-rm -rf ${REGISTRY_INSTALL_PATH} &>/dev/null
+# Delete existing duplicate data
+files=(
+    "/etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.ca.crt"
+    "${REGISTRY_INSTALL_PATH}"
+)
+for file in "${files[@]}"; do
+    if [ -e "$file" ]; then
+        rm -rf "$file" 2>/dev/null
+        if [ $? -eq 0 ]; then
+            echo "ok: [delete existing duplicate data: $file]"
+        fi
+    else
+        echo "skipping: [delete existing duplicate data]"
+    fi
+done
 
-
+# === Task: Install mirror registry ===
+PRINT_TASK "[TASK: Install mirror registry]"
 
 # Function to check command success and display appropriate message
-check_command_result() {
+mirror_registry_command() {
     if [ $? -eq 0 ]; then
         echo "ok: $1"
     else
@@ -39,133 +63,42 @@ check_command_result() {
     fi
 }
 
+# Create installation directory
+mkdir -p ${REGISTRY_INSTALL_PATH}
+mirror_registry_command "[create installation directory]"
+sleep 3
 
-# === Task: Generate a self-signed certificate ===
-PRINT_TASK "[TASK: Generate a self-signed certificate]"
+# Download mirror-registry
+wget -P ${REGISTRY_INSTALL_PATH} https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/latest/mirror-registry.tar.gz &> /dev/null
+mirror_registry_command "[download mirror-registry package]"
 
-# Generate a directory for creating certificates
-mkdir -p ${REGISTRY_CERT_PATH} &>/dev/null
-check_command_result "[created directory for certificates: ${REGISTRY_CERT_PATH}]"
+# Extract the downloaded mirror-registry package
+tar xvf ${REGISTRY_INSTALL_PATH}/mirror-registry.tar.gz -C ${REGISTRY_INSTALL_PATH}/ &> /dev/null
+mirror_registry_command "[extract the downloaded mirror-registry package]"
 
-# Generate the root Certificate Authority (CA) key
-openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key 4096 &>/dev/null
-check_command_result "[generated root certificate authority key]"
+# Install mirror-registry
+cd ${REGISTRY_INSTALL_PATH}
+${REGISTRY_INSTALL_PATH}/mirror-registry install -v \
+     --quayHostname ${REGISTRY_DOMAIN} --quayRoot ${REGISTRY_INSTALL_PATH}/ \
+     --initUser ${USER} --initPassword ${PASSWD}
+mirror_registry_command "[installing mirror-registry...]"
 
-# Generate the root CA certificate
-openssl req -x509 \
-  -new -nodes \
-  -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key \
-  -sha256 -days 36500 \
-  -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt \
-  -subj /CN="Local Red Hat Signer" \
-  -reqexts SAN \
-  -extensions SAN \
-  -config <(cat /etc/pki/tls/openssl.cnf \
-      <(printf '[SAN]\nbasicConstraints=critical, CA:TRUE\nkeyUsage=keyCertSign, cRLSign, digitalSignature')) &>/dev/null
-check_command_result "[generate the root CA certificate]"
+# Wait for the installation to complete
+cd ~
+sleep 6
 
-# Generate the domain key
-openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key 2048 &>/dev/null
-check_command_result "[generate the domain key]"
+# Get the status and number of containers for quay-pod
+podman pod ps | grep -P '(?=.*\bquay-pod\b)(?=.*\bRunning\b)(?=.*\b4\b)'
+mirror_registry_command "[mirror registry Pod is running]"
 
-# Generate a certificate signing request (CSR) for the domain
-openssl req -new -sha256 \
-    -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key \
-    -subj "/O=Local Red Hat CodeReady Workspaces/CN=${REGISTRY_DOMAIN}" \
-    -reqexts SAN \
-    -config <(cat /etc/pki/tls/openssl.cnf \
-        <(printf "\n[SAN]\nsubjectAltName=DNS:${REGISTRY_DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth")) \
-    -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.csr &>/dev/null
-check_command_result "[generate the certificate signing request for the domain(CSR)]"
+# Copy the rootCA certificate to the trusted source
+cp ${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.ca.pem
+mirror_registry_command "[copy the rootCA certificate to the trusted source: /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.ca.pem]"
 
-# Generate the domain certificate (CRT)
-openssl x509 -req -sha256 \
-    -extfile <(printf "subjectAltName=DNS:${REGISTRY_DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth") \
-    -days 36500 \
-    -in ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.csr \
-    -CA ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt \
-    -CAkey ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key \
-    -CAcreateserial -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt &>/dev/null
-check_command_result "[generate the domain certificate(CRT)]"
-
-
-
-# === Task: Install Registry ===
-PRINT_TASK "[TASK: Install registry]"
-
-# Created directories for registry installation
-mkdir -p ${REGISTRY_INSTALL_PATH}/{auth,certs,data} &>/dev/null
-check_command_result "[create directories for registry installation ${REGISTRY_INSTALL_PATH}]"
-
-# Copying root and domain certificates to trust source
-cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt" /etc/pki/ca-trust/source/anchors/ &>/dev/null
-check_command_result "[copying root and domain certificates to trust source]"
-
-# Copy certificate and key to the specified path
-cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt" ${REGISTRY_INSTALL_PATH}/certs/ &>/dev/null
-check_command_result "[copy certificate and key to ${REGISTRY_INSTALL_PATH}/certs/]"
-
-# Update trust settings with new certificates
-update-ca-trust &>/dev/null
-check_command_result "[updating trust settings with new certificates]"
-
-# Create user using htpasswd identity provider
-htpasswd -bBc ${REGISTRY_INSTALL_PATH}/auth/htpasswd "${USER}" "${PASSWD}" &>/dev/null
-check_command_result "[create ${USER} user using htpasswd identity provider]" 
-
-# Delete the container with the same name
-podman stop $CONTAINER_NAME &>/dev/null
-podman rm $CONTAINER_NAME &>/dev/null
-
-#  Generate registry container
-podman run \
-    --name ${CONTAINER_NAME} \
-    -p 5000:5000 \
-    -e REGISTRY_AUTH="htpasswd" \
-    -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
-    -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-    -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/${REGISTRY_DOMAIN}.crt \
-    -e REGISTRY_HTTP_TLS_KEY=/certs/${REGISTRY_DOMAIN}.key \
-    -e REGISTRY_STORAGE_DELETE_ENABLED=true \
-    -v ${REGISTRY_INSTALL_PATH}/data:/var/lib/registry:z \
-    -v ${REGISTRY_INSTALL_PATH}/auth:/auth:z \
-    -v ${REGISTRY_INSTALL_PATH}/certs:/certs:z \
-    -d docker.io/library/registry:2 &>/dev/null
-
-sudo sleep 60
-
-# Check if container is running
-container_info=$(podman inspect -f '{{.State.Status}}' $CONTAINER_NAME 2>/dev/null)
-if [ "$container_info" == "running" ]; then
-    echo "ok: [container '$CONTAINER_NAME' is running]"
-else
-    echo "failed: [container '$CONTAINER_NAME' is not running]"
-fi
-
-
-# Generating a systemd unit file for the registry service
-rm -rf /etc/systemd/system/${CONTAINER_NAME}.service
-
-cat << EOF > /etc/systemd/system/${CONTAINER_NAME}.service
-[Unit]
-Description= ${CONTAINER_NAME} service
-After=network.target
-After=network-online.target
-[Service]
-Restart=always
-ExecStart=/usr/bin/podman start -a ${CONTAINER_NAME}
-ExecStop=/usr/bin/podman stop -t 10 ${CONTAINER_NAME}
-[Install]
-WantedBy=multi-user.target
-EOF
-check_command_result "[generating a systemd unit file for the ${CONTAINER_NAME} service]"
-
-# Enable and start registry service
-systemctl enable ${CONTAINER_NAME}.service &>/dev/null
-check_command_result "[change the systemd ${CONTAINER_NAME} service to enable]"
-systemctl start ${CONTAINER_NAME}.service
-check_command_result "[restart the systemd ${CONTAINER_NAME} service]"
+# Trust the rootCA certificate
+update-ca-trust
+mirror_registry_command "[trust the rootCA certificate]"
 
 # loggin registry
-podman login -u ${USER} -p ${PASSWD} https://${REGISTRY_DOMAIN}:5000 &>/dev/null
-check_command_result "[login ${CONTAINER_NAME}: https://${REGISTRY_DOMAIN}:5000]"
+podman login -u ${USER} -p ${PASSWD} https://${REGISTRY_DOMAIN}:8443 &>/dev/null
+mirror_registry_command  "[test login https://${REGISTRY_DOMAIN}:8443]"
