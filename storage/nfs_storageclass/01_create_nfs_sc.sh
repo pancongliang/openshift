23d8aedce8a6cc133994eceab3e21759803d5560
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-09-07 01:42:06 +0800

    Create 01_create_nfs_sc.sh

---
+++ storage/nfs_storageclass/01_create_nfs_sc.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+# === Function to print a task with uniform length ===
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=90  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
+
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
+}
+# ====================================================
+
+# Set your namespace, NFS server IP, and NFS server path here
+NAMESPACE="nfs-client-provisioner"
+NFS_SERVER_IP="10.74.251.171"
+NFS_DIR="/nfs"
+
+
+# Function to check command success and display appropriate message
+run_command() {
+    if [ $? -eq 0 ]; then
+        echo "ok: $1"
+    else
+        echo "failed: $1"
+    fi
+}
+
+
+# === Task: Setup nfs services ===
+PRINT_TASK "[TASK: Setup nfs services]"
+
+# install nfs-utils
+yum install nfs-utils -y &> /dev/null
+run_command "[install nfs-utils package]"
+
+# Create NFS directories
+rm -rf ${NFS_DIR}
+mkdir -p ${NFS_DIR}
+run_command "[create nfs director: ${NFS_DIR}]"
+
+# Add nfsnobody user if not exists
+if id "nfsnobody" &>/dev/null; then
+    echo "skipping: [nfsnobody user exists]"
+else
+    useradd nfsnobody
+    echo "ok: [add nfsnobody user]"
+fi
+
+# Change ownership and permissions
+chown -R nfsnobody.nfsnobody ${NFS_DIR}
+run_command "[changing ownership of an NFS directory]"
+
+chmod -R 777 ${NFS_DIR}
+run_command "[change NFS directory permissions]"
+
+# Add NFS export configuration
+export_config_line="${NFS_PATH}    (rw,sync,no_wdelay,no_root_squash,insecure,fsid=0)"
+if grep -q "$export_config_line" "/etc/exports"; then
+    echo "skipping: [nfs export configuration already exists]"
+else
+    echo "$export_config_line" >> "/etc/exports"
+    echo "ok: [add nfs export configuration]"
+fi
+
+
+
+# === Task: Install NFS storage class ===
+PRINT_TASK "[TASK: Install NFS storage class]"
+
+# new-project
+oc new-project $NAMESPACE
+run_command "[create new project: $NAMESPACE]"
+
+# rbac
+cat << EOF | oc apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: nfs-client-provisioner
+  # replace with namespace where provisioner is deployed
+  namespace: $NAMESPACE
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: nfs-client-provisioner-runner
+rules:
+  - apiGroups: [""]
+    resources: ["persistentvolumes"]
+    verbs: ["get", "list", "watch", "create", "delete"]
+  - apiGroups: [""]
+    resources: ["persistentvolumeclaims"]
+    verbs: ["get", "list", "watch", "update"]
+  - apiGroups: ["storage.k8s.io"]
+    resources: ["storageclasses"]
+    verbs: ["get", "list", "watch"]
+  - apiGroups: [""]
+    resources: ["events"]
+    verbs: ["create", "update", "patch"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: run-nfs-client-provisioner
+subjects:
+  - kind: ServiceAccount
+    name: nfs-client-provisioner
+    # replace with namespace where provisioner is deployed
+    namespace: $NAMESPACE
+roleRef:
+  kind: ClusterRole
+  name: nfs-client-provisioner-runner
+  apiGroup: rbac.authorization.k8s.io
+---
+kind: Role
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: leader-locking-nfs-client-provisioner
+  # replace with namespace where provisioner is deployed
+  namespace: $NAMESPACE
+rules:
+  - apiGroups: [""]
+    resources: ["endpoints"]
+    verbs: ["get", "list", "watch", "create", "update", "patch"]
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: leader-locking-nfs-client-provisioner
+  # replace with namespace where provisioner is deployed
+  namespace: $NAMESPACE
+subjects:
+  - kind: ServiceAccount
+    name: nfs-client-provisioner
+    # replace with namespace where provisioner is deployed
+    namespace: $NAMESPACE
+roleRef:
+  kind: Role
+  name: leader-locking-nfs-client-provisioner
+  apiGroup: rbac.authorization.k8s.io
+EOF  
+run_command "[create rbac configuration]"
+
+# scc
+oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE:nfs-client-provisioner
+run_command "[add SCC to user]"
+
+# deployment
+cat << EOF | oc apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nfs-client-provisioner
+  labels:
+    app: nfs-client-provisioner
+  # replace with namespace where provisioner is deployed
+  namespace: $NAMESPACE
+spec:
+  replicas: 1
+  strategy:
+    type: Recreate
+  selector:
+    matchLabels:
+      app: nfs-client-provisioner
+  template:
+    metadata:
+      labels:
+        app: nfs-client-provisioner
+    spec:
+      serviceAccountName: nfs-client-provisioner
+      containers:
+        - name: nfs-client-provisioner
+          image: k8s.gcr.io/sig-storage/nfs-subdir-external-provisioner:v4.0.2
+          volumeMounts:
+            - name: nfs-client-root
+              mountPath: /persistentvolumes
+          env:
+            - name: PROVISIONER_NAME
+              value: nfs-storage-provisioner
+            - name: NFS_SERVER
+              value: $NFS_SERVER_IP
+            - name: NFS_PATH_PATH
+              value: $NFS_DIR
+      volumes:
+        - name: nfs-client-root
+          nfs:
+            server: $NFS_SERVER_IP
+            path: $NFS_DIR
+EOF
+run_command "[deploy nfs-client-provisioner]"
+
+# storage class
+cat << EOF | oc apply -f -
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: nfs-storage
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+provisioner: nfs-storage-provisioner     
+parameters:
+  archiveOnDelete: "false"
+  reclaimPolicy: Retain
+EOF
+run_command "[create storage class]"
