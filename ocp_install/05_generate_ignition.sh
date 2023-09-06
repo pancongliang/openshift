ae7ce715c31a94ab4f7c9b7f28df6d22e75d8eb8
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-09-06 19:49:58 +0800

    Update 05_generate_ignition.sh

--- ocp_install/05_generate_ignition.sh
+++ ocp_install/05_generate_ignition.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
-#######################################################
-
+# === Function to print a task with uniform length ===
 # Function to print a task with uniform length
 PRINT_TASK() {
-    max_length=45  # Adjust this to your desired maximum length
+    max_length=90  # Adjust this to your desired maximum length
     task_title="$1"
     title_length=${#task_title}
     stars=$((max_length - title_length))
 
     echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
 }
-
-#######################################################
 
 # Task: Generate a defined install-config file
 PRINT_TASK "[TASK: Generate a defined install-config file]"
@@ -27,24 +23,22 @@ run_command() {
 }
 
 # Backup and format the registry CA certificate
-cp "${REGISTRY_CERT_PATH}/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.crt" "${REGISTRY_CERT_PATH}/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.crt.bak"
+rm -rf "${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak"
+cp "${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem" "${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak"
 run_command "[backup registry CA certificate]"
 
-sed -i 's/^/  /' "${REGISTRY_CA_FILE.bak}"
+sed -i 's/^/  /' "${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak"
 run_command "[format registry ca certificate]"
 
 
 # Create ssh-key for accessing CoreOS
 rm -rf ${SSH_KEY_PATH}
-mkdir -p ${SSH_KEY_PATH}
-run_command "[create ${SSH_KEY_PATH} directory]"
-
-ssh-keygen -N '' -f ${SSH_PRI_FILE}		
+ssh-keygen -N '' -f ${SSH_KEY_PATH}/id_rsa &> /dev/null
 run_command "[create ssh-key for accessing coreos]"
 
 
 # Define variables
-export REGISTRY_CA_CERT_FORMAT="$(cat ${REGISTRY_CERT_PATH}/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.crt.bak)"
+export REGISTRY_CA_CERT_FORMAT="$(cat ${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak)"
 export REGISTRY_AUTH=$(echo -n "${REGISTRY_ID}:${REGISTRY_PW}" | base64)
 export SSH_PUB_STR="$(cat ${SSH_KEY_PATH}/id_rsa.pub)"
 
@@ -89,8 +83,8 @@ EOF
 run_command "[create install-config.yaml fole]"
 
 # Delete certificate
-rm -rf "${REGISTRY_CERT_PATH}/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.crt.bak"
-run_command "[delete ${REGISTRY_CERT_PATH}/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.crt.bak file]"
+rm -rf ${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak
+run_command "[delete ${REGISTRY_INSTALL_PATH}/quay-rootCA/rootCA.pem.bak file]"
 
 # Add an empty line after the task
 echo
@@ -111,16 +105,16 @@ cp "$HTTPD_PATH/install-config.yaml" "${IGNITION_PATH}"
 run_command "[copy the install-config.yaml file to the installation directory]"
 
 # Generate manifests
-openshift-install create manifests --dir "${IGNITION_PATH}"
+openshift-install create manifests --dir "${IGNITION_PATH}" &> /dev/null
 run_command "[generate manifests]"
 
 # Check if the file contains "mastersSchedulable: true"
 if grep -q "mastersSchedulable: true" "${IGNITION_PATH}/manifests/cluster-scheduler-02-config.yml"; then
   # Replace "mastersSchedulable: true" with "mastersSchedulable: false"
-  sed -i 's/mastersSchedulable: true/mastersSchedulable: false/' "${IGNITION_PATH}/manifests/cluster-scheduler-02-config.yml""
-  echo "'ok: [disable the master node from scheduling custom pods]"
+  sed -i 's/mastersSchedulable: true/mastersSchedulable: false/' "${IGNITION_PATH}/manifests/cluster-scheduler-02-config.yml"
+  echo "ok: [disable the master node from scheduling custom pods]"
 else
-  echo "'failed: mastersSchedulable: true' not found, no changes made"
+  echo "skipping: [scheduling of custom pods on master nodes is already disabled]"
 fi
 
 # Add an empty line after the task
@@ -134,7 +128,7 @@ echo
 PRINT_TASK "[TASK: Generate default ignition file]"
 
 # Generate and modify ignition configuration files
-openshift-install create ignition-configs --dir "${IGNITION_PATH}"
+openshift-install create ignition-configs --dir "${IGNITION_PATH}" &> /dev/null
 run_command "[generate default ignition file]"
 
 # Add an empty line after the task
@@ -146,7 +140,8 @@ PRINT_TASK "[TASK: Generate an ignition file containing the node hostname]"
 # Copy ignition files with appropriate hostnames
 BOOTSTRAP_HOSTNAME="${BOOTSTRAP_HOSTNAME}"
 MASTER_HOSTNAMES=("${MASTER01_HOSTNAME}" "${MASTER02_HOSTNAME}" "${MASTER03_HOSTNAME}")
-WORKER_HOSTNAMES=("${WORKER01_HOSTNAME" "${WORKER01_HOSTNAME}")
+WORKER_HOSTNAMES=("${WORKER01_HOSTNAME}" "${WORKER02_HOSTNAME}")
+
 
 cp "${IGNITION_PATH}/bootstrap.ign" "${IGNITION_PATH}/${BOOTSTRAP_HOSTNAME}-bak.ign"
 run_command "[copy and customize the bootstrap.ign file name: ${BOOTSTRAP_HOSTNAME}-bak.ign]"
@@ -162,17 +157,17 @@ for WORKER_HOSTNAME in "${WORKER_HOSTNAMES[@]}"; do
 done
 
 # Update hostname in ignition files
-sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'${BOOTSTRAP_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}'"}"},"mode":420}]}}/' "${IGNITION_PATH}/${BOOTSTRAP_HOSTNAME}.ign"
+sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'"${BOOTSTRAP_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}"'"},"mode":420}]}}/' "${IGNITION_PATH}/${BOOTSTRAP_HOSTNAME}.ign"
 run_command "[add the appropriate hostname field to the ${BOOTSTRAP_HOSTNAME}.ign file]"
 
 for MASTER_HOSTNAME in "${MASTER_HOSTNAMES[@]}"; do
-    sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'${MASTER_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}'"}"},"mode":420}]}}/' "${IGNITION_PATH}/${MASTER_HOSTNAME}.ign"
+    sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'"${MASTER_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}"'"},"mode":420}]}}/' "${IGNITION_PATH}/${MASTER_HOSTNAME}.ign"
     run_command "[add the appropriate hostname field to the ${MASTER_HOSTNAME}.ign file]"
 done
 
 for WORKER_HOSTNAME in "${WORKER_HOSTNAMES[@]}"; do
-    sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'${WORKER_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}'"}"},"mode":420}]}}/' "${IGNITION_PATH}/${WORKER_HOSTNAME}.ign"
-    run_command "[add the appropriate hostname field to the ${MASTER_HOSTNAME}.ign file]"
+    sed -i 's/}$/,"storage":{"files":[{"path":"\/etc\/hostname","contents":{"source":"data:,'"${WORKER_HOSTNAME}.${CLUSTER_NAME}.${BASE_DOMAIN}"'"},"mode":420}]}}/' "${IGNITION_PATH}/${WORKER_HOSTNAME}.ign"
+    run_command "[add the appropriate hostname field to the ${WORKER_HOSTNAME}.ign file]"
 done
 
 # Set correct permissions
