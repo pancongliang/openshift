119684acccb312c86069d01dcaa61a207ee3ee4e
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-09-06 17:34:05 +0800

    Update 04_mirror_ocp_image.sh

--- ocp_install/04_mirror_ocp_image.sh
+++ ocp_install/04_mirror_ocp_image.sh
@@ -16,7 +16,7 @@ PRINT_TASK() {
 PRINT_TASK "[TASK: Mirror ocp image to mirror-registry]"
 
 # Login to the registry
-podman login -u "$REGISTRY_ID" -p "$REGISTRY_PW" --authfile "${PULL_SECRET}" "${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:8443"
+podman login -u "$REGISTRY_ID" -p "$REGISTRY_PW" --authfile "${PULL_SECRET}" "${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:8443" &>/dev/null
 
 # Check the return code of the podman login command
 if [ $? -eq 0 ]; then
@@ -26,10 +26,10 @@ else
 fi
 
 # Execute oc adm release mirror command
-oc adm -a ${{PULL_SECRET}} release mirror \
+oc adm -a ${PULL_SECRET} release mirror \
   --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} \
   --to=${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:8443/${LOCAL_REPOSITORY} \
-  --to-release-image=${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:8443/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 
+  --to-release-image=${REGISTRY_HOSTNAME}.${BASE_DOMAIN}:8443/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} &>/dev/null
   
 # Check the return code of the oc adm release mirror command
 if [ $? -eq 0 ]; then
