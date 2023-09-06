15f36b26eda733cdd67cf8e0892d69f2407ff7d9
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-09-06 16:06:46 +0800

    Update 06_prepare_autoconfiguration_files_for_coreos.sh.sh

--- ocp_install/06_prepare_autoconfiguration_files_for_coreos.sh.sh
+++ ocp_install/06_prepare_autoconfiguration_files_for_coreos.sh.sh
@@ -1,6 +1,31 @@
-echo ====== Prepare autoconfiguration files for CoreOS ======
-
 #!/bin/bash
+
+#######################################################
+
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=45  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
+
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
+}
+
+#######################################################
+
+# Task: Generate setup script file
+PRINT_TASK "[TASK: Generate setup script file]"
+
+# Function to check command success and display appropriate message
+run_command() {
+    if [ $? -eq 0 ]; then
+        echo "ok: $1"
+    else
+        echo "failed: $1"
+    fi
+}
+
 
 # Function to generate setup script for a node
 generate_setup_script() {
@@ -8,7 +33,7 @@ generate_setup_script() {
     local IP_ADDRESS=$2
 
     # Generate a setup script for the node
-    cat << EOF > ${IGNITION_PATH}/set-${HOSTNAME}.sh
+    cat << EOF > "${IGNITION_PATH}/set-${HOSTNAME}.sh"
 #!/bin/bash
 # Configure network settings
 nmcli con mod ${NET_IF_NAME} ipv4.addresses ${IP_ADDRESS}/${NETMASK} ipv4.gateway ${GATEWAY_IP} ipv4.dns ${DNS_IP} ipv4.method manual connection.autoconnect yes
@@ -21,38 +46,23 @@ sudo sleep 10
 sudo coreos-installer install ${COREOS_INSTALL_DEV} --insecure-ignition --ignition-url=http://${BASTION_IP}:8080/pre/${HOSTNAME}.ign --insecure-ignition --firstboot-args 'rd.neednet=1' --copy-network
 EOF
 
-    # Make the script executable
-    chmod +x ${IGNITION_PATH}/set-${HOSTNAME}.sh
+    # Check if the setup script file was successfully generated
+    if [ -f "${IGNITION_PATH}/set-${HOSTNAME}.sh" ]; then
+        echo "ok: [generate setup script: ${IGNITION_PATH}/set-${HOSTNAME}.sh]"
+    else
+        echo "failed: [generate setup script for ${HOSTNAME}"
+    fi
 }
 
 # Generate setup scripts for each node
-generate_setup_script ${BOOTSTRAP_HOSTNAME} ${BOOTSTRAP_IP}
-generate_setup_script ${MASTER01_HOSTNAME} ${MASTER01_IP}
-generate_setup_script ${MASTER02_HOSTNAME} ${MASTER02_IP}
-generate_setup_script ${MASTER03_HOSTNAME} ${MASTER03_IP}
-generate_setup_script ${WORKER01_HOSTNAME} ${WORKER01_IP}
-generate_setup_script ${WORKER02_HOSTNAME} ${WORKER02_IP}
+generate_setup_script "${BOOTSTRAP_HOSTNAME}" "${BOOTSTRAP_IP}"
+generate_setup_script "${MASTER01_HOSTNAME}" "${MASTER01_IP}"
+generate_setup_script "${MASTER02_HOSTNAME}" "${MASTER02_IP}"
+generate_setup_script "${MASTER03_HOSTNAME}" "${MASTER03_IP}"
+generate_setup_script "${WORKER01_HOSTNAME}" "${WORKER01_IP}"
+generate_setup_script "${WORKER02_HOSTNAME}" "${WORKER02_IP}"
 
-# Check if files were generated successfully
-generated_files=("set-${BOOTSTRAP_HOSTNAME}.sh"
-                 "set-${MASTER01_HOSTNAME}.sh"
-                 "set-${MASTER02_HOSTNAME}.sh"
-                 "set-${MASTER03_HOSTNAME}.sh"
-                 "set-${WORKER01_HOSTNAME}.sh"
-                 "set-${WORKER02_HOSTNAME}.sh")
 
-success=true
-
-for file in "${generated_files[@]}"; do
-    if [ ! -f "${IGNITION_PATH}/${file}" ]; then
-        echo "Error: ${file} CoreOS configuration file generated failed."
-        success=false
-    fi
-done
-
-if [ "${success}" = true ]; then
-    echo "CoreOS configuration file generated successfully."
-fi
-
-# Display generated files
-ls -l "${IGNITION_PATH}"/*.sh
+# Make the script executable
+chmod +x ${IGNITION_PATH}/set-${HOSTNAME}.sh
+run_command "[change ignition file permissions]"
