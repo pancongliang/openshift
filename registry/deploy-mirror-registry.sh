5403f4094a578829a1680e1f51477182bb36ec32
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-10-17 02:41:22 +0800

    Update deploy-mirror-registry.sh

--- registry/deploy-mirror-registry.sh
+++ registry/deploy-mirror-registry.sh
@@ -67,7 +67,6 @@ run_command "[create ${REGISTRY_INSTALL_DIR} directory]"
 
 mkdir -p /tmp/mirror-registry
 run_command "[create /tmp/registry-package directory]"
-sleep 1
 
 # Download mirror-registry
 wget -P /tmp/registry-package https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/latest/mirror-registry.tar.gz &> /dev/null
@@ -86,10 +85,6 @@ run_command "[extract the mirror-registry package]"
      --initUser ${REGISTRY_ID} --initPassword ${REGISTRY_PW} 
 run_command "[installing mirror-registry...]"
 
-# Wait for the installation to complete
-cd - &>/dev/null
-sleep 6
-
 # Get the status and number of containers for quay-pod
 podman pod ps | grep -P '(?=.*\bquay-pod\b)(?=.*\bRunning\b)(?=.*\b4\b)' &>/dev/null
 run_command "[mirror registry Pod is running]"
@@ -105,6 +100,13 @@ run_command "[trust the rootCA certificate]"
 # Move registry-package
 mv /tmp/registry-package ${REGISTRY_INSTALL_DIR}/
 run_command "[Move registry package to ${REGISTRY_INSTALL_DIR}]"
+
+# Delete package
+cd ${REGISTRY_INSTALL_DIR}/
+cd ..
+rm -rf pause.tar postgres.tar quay.tar redis.tar
+run_command "[delete package: pause.tar postgres.tar quay.tar redis.tar]"
+cd ~
 
 # loggin registry
 podman login -u ${REGISTRY_ID} -p ${REGISTRY_PW} https://${REGISTRY_DOMAIN_NAME}:8443 &>/dev/null
