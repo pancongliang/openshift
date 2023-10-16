fbc20649caf1c2d4dbf548654777b1ce3f9ee0c3
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-10-16 19:01:00 +0800

    Update deploy_docker_registry.sh

--- registry/deploy_docker_registry.sh
+++ registry/deploy_docker_registry.sh
@@ -12,8 +12,8 @@ PRINT_TASK() {
 # ====================================================
 
 # Delete existing file
-rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.ca.crt &>/dev/null
-rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN}.crt &>/dev/null
+rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.ca.crt &>/dev/null
+rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.crt &>/dev/null
 rm -rf ${REGISTRY_INSTALL_PATH} &>/dev/null
 rm -rf ${REGISTRY_CERT_PATH} &>/dev/null
 
@@ -36,15 +36,15 @@ mkdir -p ${REGISTRY_CERT_PATH} &>/dev/null
 check_command_result "[created directory for certificates: ${REGISTRY_CERT_PATH}]"
 
 # Generate the root Certificate Authority (CA) key
-openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key 4096 &>/dev/null
+openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.key 4096 &>/dev/null
 check_command_result "[generated root certificate authority key]"
 
 # Generate the root CA certificate
 openssl req -x509 \
   -new -nodes \
-  -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key \
+  -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.key \
   -sha256 -days 36500 \
-  -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt \
+  -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.crt \
   -subj /CN="Local Red Hat Signer" \
   -reqexts SAN \
   -extensions SAN \
@@ -53,27 +53,27 @@ openssl req -x509 \
 check_command_result "[generate the root CA certificate]"
 
 # Generate the domain key
-openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key 2048 &>/dev/null
+openssl genrsa -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.key 2048 &>/dev/null
 check_command_result "[generate the domain key]"
 
 # Generate a certificate signing request (CSR) for the domain
 openssl req -new -sha256 \
-    -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key \
-    -subj "/O=Local Red Hat CodeReady Workspaces/CN=${REGISTRY_DOMAIN}" \
+    -key ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.key \
+    -subj "/O=Local Red Hat CodeReady Workspaces/CN=${REGISTRY_DOMAIN_NAME}" \
     -reqexts SAN \
     -config <(cat /etc/pki/tls/openssl.cnf \
-        <(printf "\n[SAN]\nsubjectAltName=DNS:${REGISTRY_DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth")) \
-    -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.csr &>/dev/null
+        <(printf "\n[SAN]\nsubjectAltName=DNS:${REGISTRY_DOMAIN_NAME}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth")) \
+    -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.csr &>/dev/null
 check_command_result "[generate the certificate signing request for the domain(CSR)]"
 
 # Generate the domain certificate (CRT)
 openssl x509 -req -sha256 \
-    -extfile <(printf "subjectAltName=DNS:${REGISTRY_DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth") \
+    -extfile <(printf "subjectAltName=DNS:${REGISTRY_DOMAIN_NAME}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth") \
     -days 36500 \
-    -in ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.csr \
-    -CA ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt \
-    -CAkey ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.key \
-    -CAcreateserial -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt &>/dev/null
+    -in ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.csr \
+    -CA ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.crt \
+    -CAkey ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.key \
+    -CAcreateserial -out ${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.crt &>/dev/null
 check_command_result "[generate the domain certificate(CRT)]"
 
 
@@ -86,11 +86,11 @@ mkdir -p ${REGISTRY_INSTALL_PATH}/{auth,certs,data} &>/dev/null
 check_command_result "[create directories for registry installation ${REGISTRY_INSTALL_PATH}]"
 
 # Copying root and domain certificates to trust source
-cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.ca.crt" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt" /etc/pki/ca-trust/source/anchors/ &>/dev/null
+cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.ca.crt" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.crt" /etc/pki/ca-trust/source/anchors/ &>/dev/null
 check_command_result "[copying root and domain certificates to trust source]"
 
 # Copy certificate and key to the specified path
-cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.key" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN}.crt" ${REGISTRY_INSTALL_PATH}/certs/ &>/dev/null
+cp "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.key" "${REGISTRY_CERT_PATH}/${REGISTRY_DOMAIN_NAME}.crt" ${REGISTRY_INSTALL_PATH}/certs/ &>/dev/null
 check_command_result "[copy certificate and key to ${REGISTRY_INSTALL_PATH}/certs/]"
 
 # Update trust settings with new certificates
@@ -112,8 +112,8 @@ podman run \
     -e REGISTRY_AUTH="htpasswd" \
     -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
     -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-    -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/${REGISTRY_DOMAIN}.crt \
-    -e REGISTRY_HTTP_TLS_KEY=/certs/${REGISTRY_DOMAIN}.key \
+    -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/${REGISTRY_DOMAIN_NAME}.crt \
+    -e REGISTRY_HTTP_TLS_KEY=/certs/${REGISTRY_DOMAIN_NAME}.key \
     -e REGISTRY_STORAGE_DELETE_ENABLED=true \
     -v ${REGISTRY_INSTALL_PATH}/data:/var/lib/registry:z \
     -v ${REGISTRY_INSTALL_PATH}/auth:/auth:z \
@@ -155,5 +155,5 @@ systemctl start ${CONTAINER_NAME}.service
 check_command_result "[restart the systemd ${CONTAINER_NAME} service]"
 
 # loggin registry
-podman login -u ${USER} -p ${PASSWD} https://${REGISTRY_DOMAIN}:5000 &>/dev/null
-check_command_result "[login ${CONTAINER_NAME}: https://${REGISTRY_DOMAIN}:5000]"
+podman login -u ${USER} -p ${PASSWD} https://${REGISTRY_DOMAIN_NAME}:5000 &>/dev/null
+check_command_result "[login ${CONTAINER_NAME}: https://${REGISTRY_DOMAIN_NAME}:5000]"
