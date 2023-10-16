8f5908d3efa5658779f9f01ababf2f99d22dea85
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-10-17 00:57:56 +0800

    Update 00-download-scripts.sh

--- ocp-install/00-download-scripts.sh
+++ ocp-install/00-download-scripts.sh
@@ -2,19 +2,19 @@
 
 # Declare an array of scripts
 scripts=(
-    "00_security.sh"
-    "00_subscription.sh"
-    "01_ocp_env_parameter.sh"
-    "02_install_infrastructure.sh"
-    "03_install_mirror_registry.sh"
-    "04_mirror_ocp_image.sh"
-    "05_generate_ignition.sh"
-    "06_generate_setup_script_file.sh"
-    "07_configure_after_installation.sh"
+    "00-security.sh"
+    "00-subscription.sh"
+    "01-ocp-env-parameter.sh"
+    "02-install-infrastructure.sh"
+    "03-install-mirror-registry.sh"
+    "04-mirror-ocp-image.sh"
+    "05-generate-ignition.sh"
+    "06-generate-setup-script-file.sh"
+    "07-configure-after-installation.sh"
 )
 
 # Specify the base URL of the GitHub repository
-base_url="https://raw.githubusercontent.com/pancongliang/openshift/main/ocp_install/"
+base_url="https://raw.githubusercontent.com/pancongliang/openshift/main/ocp-install/"
 
 # Use a loop to download the scripts
 for script in "${scripts[@]}"; do
