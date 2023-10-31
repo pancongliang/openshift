b7c5f654ff7d3aac697d73a1d88a42138ceb59de
pancongliang <75927592+pancongliang@users.noreply.github.com>
2023-10-31 15:22:48 +0800

    Update 02-install-infrastructure.sh

--- ocp-install/02-install-infrastructure.sh
+++ ocp-install/02-install-infrastructure.sh
@@ -673,7 +673,7 @@ hostnames=(
     "${WORKER01_IP}"
     "${WORKER02_IP}"
     "${BOOTSTRAP_IP}"
-    "${NSLOOKUP_PUBLIC}"
+    "${NSLOOKUP_PUBLIP}"
 )
 
 # Loop through hostnames and perform nslookup
