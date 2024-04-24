d5ef76b2e313c949bb1a1d4169f55a0c70075f47
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-04-24 22:38:10 +0800

    Update 01-install-pre.sh

--- installing/aws-ipi/online-install-cluster-on-aws/01-install-pre.sh
+++ installing/aws-ipi/online-install-cluster-on-aws/01-install-pre.sh
@@ -26,8 +26,8 @@ PRINT_TASK "[TASK: Install AWS CLI]"
 
 # Function to install AWS CLI on Linux
 install_awscli_linux() {
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
+    curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null 
+    unzip awscliv2.zip > /dev/null 
     sudo ./aws/install &>/dev/null || true
     run_command "[Install AWS CLI]"
     sudo rm -rf aws awscliv2.zip
@@ -35,14 +35,10 @@ install_awscli_linux() {
 
 # Function to install AWS CLI on macOS
 install_awscli_mac() {
-    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-    sudo installer -pkg AWSCLIV2.pkg -target / || true
+    curl -s "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg" > /dev/null 
+    sudo installer -pkg AWSCLIV2.pkg -target / &>/dev/null || true
     run_command "[Install AWS CLI]"
-}
-
-# Function to print a task with uniform length
-run_command() {
-    echo "$1"
+    sudo rm -rf AWSCLIV2.pkg
 }
 
 # Detecting the operating system
@@ -62,13 +58,14 @@ echo
 
 # === Task: Set up AWS credentials ===
 PRINT_TASK "[TASK: Set up AWS credentials]"
-
+rm -rf $HOME/.aws
+mkdir -p $HOME/.aws
 cat << EOF > "$HOME/.aws/credentials"
 [default]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
-run_command "[TASK: Set up AWS credentials]"
+run_command "[Set up AWS credentials]"
 
 # Add an empty line after the task
 echo
@@ -100,7 +97,7 @@ install_tar_gz() {
     local tool_name="$1"
     local tool_url="$2"  
     # Download the tool
-    sudo wget -P "/usr/local/bin" "$tool_url" &> /dev/null    
+    wget -P "/usr/local/bin" "$tool_url" &> /dev/null    
     if [ $? -eq 0 ]; then
         echo "ok: [download $tool_name tool]"        
         # Extract the downloaded tool
