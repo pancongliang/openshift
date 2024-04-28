1467692968f4b0454bc8b63615c86a78292db027
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-04-28 17:41:10 +0800

    Update 02-set-aws.sh

--- installing/aws-ipi/offline-install-cluster-on-aws/02-set-aws.sh
+++ installing/aws-ipi/offline-install-cluster-on-aws/02-set-aws.sh
@@ -99,7 +99,7 @@ echo
 
 
 
-# === Task: Create Subnet ===
+# === Task: Create Subnets ===
 PRINT_TASK "[TASK: Create Subnet]"
 
 # Create public subnet and get public subnet ID
@@ -153,7 +153,7 @@ PUBLIC_ROUTE_TABLE_ID=$(aws --region $REGION ec2 create-route-table --vpc-id $VP
 run_command "[Create public Route Table: $PUBLIC_ROUTE_TABLE_ID]"
 
 # Add tag to public Route Table
-aws --region $REGION ec2 create-tags --resources $PUBLIC_ROUTE_TABLE_ID --tags Key=Name,Value="$PUBLIC_RTB_NAME"
+aws --region $REGION ec2 create-tags --resources $PUBLIC_ROUTE_TABLE_ID --tags Key=Name,Value="$PUBLIC_RTB_NAME"  > /dev/null
 run_command "[Add tag to public Route Table: $PUBLIC_RTB_NAME]"
 
 # Create private Route Table
@@ -161,19 +161,19 @@ PRIVATE_ROUTE_TABLE_ID=$(aws --region $REGION ec2 create-route-table --vpc-id $V
 run_command "[Create private Route Table: $PRIVATE_ROUTE_TABLE_ID]"
 
 # Add tag to private Route Table
-aws --region $REGION ec2 create-tags --resources $PRIVATE_ROUTE_TABLE_ID --tags Key=Name,Value="$PRIVATE_RTB_NAME"
+aws --region $REGION ec2 create-tags --resources $PRIVATE_ROUTE_TABLE_ID --tags Key=Name,Value="$PRIVATE_RTB_NAME"  > /dev/null
 run_command "[Add tag to private Route Table: $PRIVATE_RTB_NAME]"
 
 # Link public Route Table to Internet Gateway
-aws --region $REGION ec2 create-route --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID --route-table-id $PUBLIC_ROUTE_TABLE_ID >/dev/null
+aws --region $REGION ec2 create-route --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID --route-table-id $PUBLIC_ROUTE_TABLE_ID > /dev/null
 run_command "[Link public Route Table to Internet Gateway]"
 
 # Associate public Route Table with public subnet
-aws --region $REGION ec2 associate-route-table --subnet-id $PUBLIC_SUBNET_ID --route-table-id $PUBLIC_ROUTE_TABLE_ID >/dev/null                 
+aws --region $REGION ec2 associate-route-table --subnet-id $PUBLIC_SUBNET_ID --route-table-id $PUBLIC_ROUTE_TABLE_ID > /dev/null       
 run_command "[Associate public Route Table with public subnet]"
 
 # Associate private Route Table with private subnet
-aws --region $REGION ec2 associate-route-table --subnet-id $PRIVATE_SUBNET_ID --route-table-id $PRIVATE_ROUTE_TABLE_ID >/dev/null
+aws --region $REGION ec2 associate-route-table --subnet-id $PRIVATE_SUBNET_ID --route-table-id $PRIVATE_ROUTE_TABLE_ID > /dev/null
 run_command "[Associate private Route Table with private subnet]"
 
 
@@ -182,8 +182,8 @@ run_command "[Associate private Route Table with private subnet]"
 echo
 # ====================================================
 
-# === Task: Create security group ===
-PRINT_TASK "[TASK: Create security group]"
+# === Task: Create Security Group ===
+PRINT_TASK "[TASK: Create Security Group]"
 
 # Create security group and get security group ID
 SECURITY_GROUP_DESCRIPTION="External SSH and all internal traffic"
@@ -191,21 +191,21 @@ SECURITY_GROUP_ID=$(aws --region $REGION ec2 create-security-group --group-name 
 run_command "[Create security group and get security group ID: $SECURITY_GROUP_ID]"
 
 # Add tag to security group
-aws --region $REGION ec2 create-tags --resources $SECURITY_GROUP_ID --tags Key=Name,Value=$SECURITY_GROUP_NAME
+aws --region $REGION ec2 create-tags --resources $SECURITY_GROUP_ID --tags Key=Name,Value=$SECURITY_GROUP_NAME > /dev/null
 run_command "[Add tag to security group: $SECURITY_GROUP_NAME]"
 
 # Add inbound rule - SSH
-aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0 >/dev/null
+aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0 > /dev/null
 run_command "[Add inbound rule - SSH]"
 
 # Add inbound rule - All traffic
-aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol all --port -1 --cidr 10.0.0.0/16 >/dev/null
+aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol all --port -1 --cidr 10.0.0.0/16 > /dev/null
 run_command "[Add inbound rule - All traffic]"
 
 # Add outbound rule - All traffic
-# aws --region $REGION ec2 authorize-security-group-egress --group-id $SECURITY_GROUP_ID --protocol all --port all --cidr 0.0.0.0/0 >/dev/null
+# aws --region $REGION ec2 authorize-security-group-egress --group-id $SECURITY_GROUP_ID --protocol all --port all --cidr 0.0.0.0/0 > /dev/null
 # run_command "[Add outbound rule - All traffic]"
-aws --region $REGION ec2 describe-security-groups --group-ids $SECURITY_GROUP_ID | grep -A 5 "IpPermissionsEgress" >/dev/null
+aws --region $REGION ec2 describe-security-groups --group-ids $SECURITY_GROUP_ID | grep -A 5 "IpPermissionsEgress" > /dev/null
 run_command "[Default existing outbound rule - All traffic]"
 
 # Add an empty line after the task
@@ -214,15 +214,15 @@ echo
 
 
 
-# === Task: Create the VPC endpoint for the s3 Service===
-PRINT_TASK "[TASK: Create the VPC endpoint for the s3 Service]"
+# === Task: Create the VPC Endpoint for the S3 Service===
+PRINT_TASK "[TASK: Create the VPC Endpoint for the S3 Service]"
 
-# Create the VPC endpoint for the s3 Service and get endpoint ID
+# Create the VPC endpoint for the S3 Service and get endpoint ID
 S3_ENDPOINT_ID=$(aws --region $REGION ec2 create-vpc-endpoint --vpc-endpoint-type Gateway --vpc-id $VPC_ID --service-name com.amazonaws.$REGION.s3 --route --route-table-ids $PRIVATE_ROUTE_TABLE_ID --no-private-dns-enabled --query 'VpcEndpoint.VpcEndpointId' --output text)
 run_command "[Create the VPC endpoint for the s3 Service: S3_ENDPOINT_ID]"
 
 # Add tag to S3 Service endpoint
-aws --region $REGION ec2 create-tags --resources $S3_ENDPOINT_ID --tags Key=Name,Value="$S3_ENDPOINT_NAME"
+aws --region $REGION ec2 create-tags --resources $S3_ENDPOINT_ID --tags Key=Name,Value="$S3_ENDPOINT_NAME" > /dev/null
 run_command "[Add tag to S3 Service endpoint: $S3_ENDPOINT_NAME]"
 
 # Add an empty line after the task
@@ -231,27 +231,27 @@ echo
 
 
 
-# === Task: Create the VPC endpoint for the ELB Service ===
-PRINT_TASK "[TASK: Create the VPC endpoint for the ELB Service]"
+# === Task: Create the VPC Endpoint for the ELB Service ===
+PRINT_TASK "[TASK: Create the VPC Endpoint for the ELB Service]"
 
 # Create the VPC endpoint for the ELB Service and get endpoint ID
 ELB_ENDPOINT_ID=$(aws --region $REGION ec2 create-vpc-endpoint --vpc-endpoint-type Interface --vpc-id ${VPC_ID} --service-name com.amazonaws.$REGION.elasticloadbalancing --subnet-ids ${PRIVATE_SUBNET_ID} --private-dns-enabled --query 'VpcEndpoint.VpcEndpointId' --output text)
 run_command "[Create the VPC endpoint for the ELB Service: $ELB_ENDPOINT_ID]"
 
 # Add tag to ELB Service endpoint
-aws --region $REGION ec2 create-tags --resources $ELB_ENDPOINT_ID --tags Key=Name,Value="$ELB_ENDPOINT_NAME"
+aws --region $REGION ec2 create-tags --resources $ELB_ENDPOINT_ID --tags Key=Name,Value="$ELB_ENDPOINT_NAME" > /dev/null
 run_command "[Add tags to EC2 Service endpoint: $ELB_ENDPOINT_NAME]"
 
 # Obtain default security group IDs associated with the ELB endpoint
-ELB_ENDPOINT_SG_DEFAULT=$(aws --region $REGION ec2 describe-vpc-endpoints --vpc-endpoint-ids $ELB_ENDPOINT_ID | jq -r '.VpcEndpoints[].Groups[].GroupId' | uniq)
+ELB_ENDPOINT_SG_DEFAULT=$(aws --region $REGION ec2 describe-vpc-endpoints --vpc-endpoint-ids $ELB_ENDPOINT_ID | jq -r '.VpcEndpoints[].Groups[].GroupId' | uniq > /dev/null)
 run_command "[Obtain default security group IDs associated with the ELB endpoint]"
 
 # Add the default security group IDs and the $SECURITY_GROUP_NAME security group ID to the ELB endpoint
-aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${ELB_ENDPOINT_ID} --add-security-group-ids ${ELB_ENDPOINT_SG_DEFAULT} --add-security-group-ids ${SECURITY_GROUP_ID}
+aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${ELB_ENDPOINT_ID} --add-security-group-ids ${ELB_ENDPOINT_SG_DEFAULT} --add-security-group-ids ${SECURITY_GROUP_ID} > /dev/null 
 run_command "[Add default security groups and provided security group to ELB endpoint]"
 
 # Remove default security group IDs from the ELB endpoint
-aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${ELB_ENDPOINT_ID} --remove-security-group-ids ${ELB_ENDPOINT_SG_DEFAULT}
+aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${ELB_ENDPOINT_ID} --remove-security-group-ids ${ELB_ENDPOINT_SG_DEFAULT} > /dev/null
 run_command "[Remove default security group IDs from ELB endpoint]"
 
 # Add an empty line after the task
@@ -260,27 +260,27 @@ echo
 
 
 
-# === Task: Create the VPC endpoint for the EC2 Service ===
-PRINT_TASK "[TASK: Create the VPC endpoint for the EC2 Service]"
+# === Task: Create the VPC Endpoint for the EC2 Service ===
+PRINT_TASK "[TASK: Create the VPC Endpoint for the EC2 Service]"
 
 # Create the VPC endpoint for the EC2 Service and get endpoint ID
 EC2_ENDPOINT_ID=$(aws --region $REGION ec2 create-vpc-endpoint --vpc-endpoint-type Interface --vpc-id ${VPC_ID} --service-name com.amazonaws.$REGION.ec2 --subnet-ids $PRIVATE_SUBNET_ID --private-dns-enabled --query 'VpcEndpoint.VpcEndpointId' --output text)
 run_command "[Create the VPC endpoint for the EC2 Service: $EC2_ENDPOINT_ID]"
 
 # Add tag to EC2 Service endpoint
-aws --region $REGION ec2 create-tags --resources $EC2_ENDPOINT_ID --tags Key=Name,Value="$EC2_ENDPOINT_NAME"
+aws --region $REGION ec2 create-tags --resources $EC2_ENDPOINT_ID --tags Key=Name,Value="$EC2_ENDPOINT_NAME" > /dev/null
 run_command "[Add tags to EC2 Service endpoint: $EC2_ENDPOINT_NAME]"
 
 # Obtain default security group IDs associated with the EC2 endpoint
-EC2_ENDPOINT_SG_DEFAULT=$(aws --region $REGION ec2 describe-vpc-endpoints --vpc-endpoint-ids $EC2_ENDPOINT_ID | jq -r '.VpcEndpoints[].Groups[].GroupId' | uniq)
+EC2_ENDPOINT_SG_DEFAULT=$(aws --region $REGION ec2 describe-vpc-endpoints --vpc-endpoint-ids $EC2_ENDPOINT_ID | jq -r '.VpcEndpoints[].Groups[].GroupId' | uniq > /dev/null)
 run_command "[Obtain default security group IDs associated with the EC2 endpoint]"
 
 # Add the default security group IDs and the $SECURITY_GROUP_NAME security group ID to the EC2 endpoint
-aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${EC2_ENDPOINT_ID} --add-security-group-ids ${EC2_ENDPOINT_SG_DEFAULT} --add-security-group-ids ${SECURITY_GROUP_ID}
+aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${EC2_ENDPOINT_ID} --add-security-group-ids ${EC2_ENDPOINT_SG_DEFAULT} --add-security-group-ids ${SECURITY_GROUP_ID} > /dev/null
 run_command "[Add default security groups and provided security group to EC2 endpoint]"
 
 # Remove default security group IDs from the EC2 endpoint
-aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${EC2_ENDPOINT_ID} --remove-security-group-ids ${EC2_ENDPOINT_SG_DEFAULT}
+aws --region $REGION ec2 modify-vpc-endpoint --vpc-endpoint-id ${EC2_ENDPOINT_ID} --remove-security-group-ids ${EC2_ENDPOINT_SG_DEFAULT} > /dev/null
 run_command "[Remove default security group IDs from EC2 endpoint]"
 
 # Add an empty line after the task
@@ -289,8 +289,8 @@ echo
 
 
 
-# === Task: Create private hosted zone===
-PRINT_TASK "[TASK: Create private hosted zone]"
+# === Task: Create Private Hosted Zone ===
+PRINT_TASK "[TASK: Create Private Hosted Zone]"
 
 # Create private hosted zone
 HOSTED_ZONE_ID=$(aws --region $REGION route53 create-hosted-zone \
@@ -308,12 +308,12 @@ echo
 
 
 
-# === Task: Create bastion instance ===
-PRINT_TASK "[TASK: Create bastion instance]"
+# === Task: Create Bastion Instance ===
+PRINT_TASK "[TASK: Create Bastion Instance]"
 
 # Create and download the key pair file
-rm -rf ./$KEY_PAIR_NAME.pem
-aws --region $REGION ec2 delete-key-pair --key-name $KEY_PAIR_NAME >/dev/null
+rm -rf ./$KEY_PAIR_NAME.pem > /dev/null
+aws --region $REGION ec2 delete-key-pair --key-name $KEY_PAIR_NAME > /dev/null
 aws --region $REGION ec2 create-key-pair --key-name $KEY_PAIR_NAME --query 'KeyMaterial' --output text > ./$KEY_PAIR_NAME.pem
 run_command "[Create and download the key pair file: $KEY_PAIR_NAME.pem]"
 
@@ -341,14 +341,23 @@ INSTANCE_ID=$(aws --region $REGION ec2 run-instances \
 run_command "[Create bastion ec2 instance: $INSTANCE_NAME]"
 
 # Wait for instance to be in running state
-aws --region $REGION  ec2 wait instance-status-ok --instance-ids $INSTANCE_ID
+aws --region $REGION  ec2 wait instance-status-ok --instance-ids $INSTANCE_ID > /dev/null
 run_command "[Wait for $INSTANCE_ID instance to pass the status check]"
 
-aws --region $REGION ec2 wait instance-running --instance-ids $INSTANCE_ID
+aws --region $REGION ec2 wait instance-running --instance-ids $INSTANCE_ID > /dev/null
 run_command "[Wait for $INSTANCE_ID instance to be in running state]"
 
+# Add an empty line after the task
+echo
+# ====================================================
+
+
+
+# === Task: Get access to Bastion Instance information ===
+PRINT_TASK "[TASK: Get access to Bastion Instance information]"
+
 # Modify permissions for the key pair file
-chmod 400 $KEY_PAIR_NAME.pem
+chmod 400 $KEY_PAIR_NAME.pem > /dev/null
 run_command "[Modify permissions for the key pair file: $KEY_PAIR_NAME.pem]"
 
 # Get the public IP address of the bastion ec2 instance
@@ -360,14 +369,14 @@ scp -o StrictHostKeyChecking=no -o LogLevel=ERROR -i ./$KEY_PAIR_NAME.pem ./01-s
 run_command "[Copy the installation script to the $INSTANCE_NAME]"
 
 # Create access bastion machine file in current directory
-rm -rf ./ocp-bastion.sh
+rm -rf ./ocp-bastion.sh > /dev/null
 cat << EOF > "./ocp-bastion.sh"
 ssh -o StrictHostKeyChecking=no -i "$KEY_PAIR_NAME.pem" ec2-user@"$INSTANCE_IP"
 EOF
 run_command "[Create access $INSTANCE_NAME file in current directory]"
 
 # Modify permissions for the key pair file
-chmod 777 ./ocp-bastion.sh
+chmod 777 ./ocp-bastion.sh > /dev/null
 run_command "[Modify permissions for the $INSTANCE_NAME file]"
 
 # Add an empty line after the task
