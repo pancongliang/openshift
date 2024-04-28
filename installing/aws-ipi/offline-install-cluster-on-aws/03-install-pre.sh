03f8d6bd412a8284ef29750bb2490f4d38f9e5a0
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-04-28 17:42:20 +0800

    Update 03-install-pre.sh

--- installing/aws-ipi/offline-install-cluster-on-aws/03-install-pre.sh
+++ installing/aws-ipi/offline-install-cluster-on-aws/03-install-pre.sh
@@ -74,6 +74,7 @@ rm -rf $HOME/.aws
 mkdir -p $HOME/.aws
 cat << EOF > "$HOME/.aws/credentials"
 [default]
+cli_pager=
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
