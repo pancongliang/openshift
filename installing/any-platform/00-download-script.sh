584d54f9bbfb38905a5486150ab2fdb00e06e3b5
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-08-14 11:18:13 +0800

    Update 00-download-script.sh

--- installing/any-platform/00-download-script.sh
+++ installing/any-platform/00-download-script.sh
@@ -29,7 +29,7 @@ scripts=(
 )
 
 # Specify the base URL of the GitHub repository
-base_url="https://raw.githubusercontent.com/pancongliang/openshift/main/installing/v2/"
+base_url="https://raw.githubusercontent.com/pancongliang/openshift/main/installing/any-platform/"
 
 # Function to download scripts
 download_scripts() {
