757608d077d3012f1455cc77b99d739fe47cc0a7
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-08-20 12:47:58 +0800

    Create aws-ipi-destroy.sh

---
+++ installing/aws-ipi/online/aws-ipi-destroy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=110  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
+
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
+}
+
+# Function to check command success and display appropriate message
+run_command() {
+    if [ $? -eq 0 ]; then
+        echo "ok: $1"
+    else
+        echo "failed: $1"
+    fi
+}
+# ====================================================
+
+# === Task: Set up AWS credentials ===
+PRINT_TASK "[TASK: Set up AWS credentials]"
+rm -rf $HOME/.aws
+mkdir -p $HOME/.aws
+cat << EOF > "$HOME/.aws/credentials"
+[default]
+cli_pager=
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+run_command "[Set up AWS credentials]"
+
+openshift-install destroy cluster --dir $OCP_INSTALL_DIR --log-level info
