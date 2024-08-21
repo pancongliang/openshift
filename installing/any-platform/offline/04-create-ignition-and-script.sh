4ecf3ef7c9ec00b3d1865037756b38e1efdfed7b
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-08-21 09:14:46 +0800

    Update and rename 05-create-ignition-config-file.sh to 04-create-ignition-and-script.sh

--- installing/any-platform/offline/04-create-ignition-and-script.sh
+++ installing/any-platform/offline/04-create-ignition-and-script.sh
@@ -173,3 +173,53 @@ run_command "[change ignition file permissions]"
 # Add an empty line after the task
 echo
 # ====================================================
+
+
+# Task: Generate setup script file
+PRINT_TASK "[TASK: Generate setup script file]"
+
+rm -rf ${IGNITION_PATH}/*.sh
+
+# Function to generate setup script for a node
+generate_setup_script() {
+    local HOSTNAME=$1
+    local IP_ADDRESS=$2
+
+    # Generate a setup script for the node
+    cat << EOF > "${IGNITION_PATH}/set-${HOSTNAME}.sh"
+#!/bin/bash
+# Configure network settings
+nmcli con mod ${NET_IF_NAME} ipv4.addresses ${IP_ADDRESS}/${NETMASK} ipv4.gateway ${GATEWAY_IP} ipv4.dns ${DNS_SERVER_IP} ipv4.method manual connection.autoconnect yes
+nmcli con down ${NET_IF_NAME}
+nmcli con up ${NET_IF_NAME}
+
+sudo sleep 10
+
+# Install CoreOS using Ignition
+sudo coreos-installer install ${COREOS_INSTALL_DEV} --insecure-ignition --ignition-url=http://${BASTION_IP}:8080/pre/append-${HOSTNAME}.ign --insecure-ignition --firstboot-args 'rd.neednet=1' --copy-network
+EOF
+
+    # Check if the setup script file was successfully generated
+    if [ -f "${IGNITION_PATH}/set-${HOSTNAME}.sh" ]; then
+        echo "ok: [generate setup script: ${IGNITION_PATH}/set-${HOSTNAME}.sh]"
+    else
+        echo "failed: [generate setup script for ${HOSTNAME}"
+    fi
+}
+
+# Generate setup scripts for each node
+generate_setup_script "${BOOTSTRAP_HOSTNAME}" "${BOOTSTRAP_IP}"
+generate_setup_script "${MASTER01_HOSTNAME}" "${MASTER01_IP}"
+generate_setup_script "${MASTER02_HOSTNAME}" "${MASTER02_IP}"
+generate_setup_script "${MASTER03_HOSTNAME}" "${MASTER03_IP}"
+generate_setup_script "${WORKER01_HOSTNAME}" "${WORKER01_IP}"
+generate_setup_script "${WORKER02_HOSTNAME}" "${WORKER02_IP}"
+generate_setup_script "${WORKER03_HOSTNAME}" "${WORKER03_IP}"
+
+# Make the script executable
+chmod +x ${IGNITION_PATH}/*.sh
+run_command "[change ignition file permissions]"
+
+# Add an empty line after the task
+echo
+# ====================================================
