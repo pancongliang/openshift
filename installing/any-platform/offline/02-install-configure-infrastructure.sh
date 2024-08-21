e0249b2136b1eefe124dc1fb25bfeccc5ef70663
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-08-21 09:12:40 +0800

    Update 02-install-configure-infrastructure.sh

--- installing/any-platform/offline/02-install-configure-infrastructure.sh
+++ installing/any-platform/offline/02-install-configure-infrastructure.sh
@@ -22,6 +22,96 @@ run_command() {
 # ====================================================
 
 
+# Task: Sign up for a Red Hat Subscription
+PRINT_TASK "[TASK: Sign up for a Red Hat Subscription]"
+
+# Prompt for Red Hat Subscribe UserName
+read -p "Please input the Red Hat Subscribe UserName: " USER
+
+# Prompt for Red Hat Subscribe Password securely (hidden input)
+read -s -p "Please input the Red Hat Subscribe Password: " PASSWD
+
+# Move to a new line after password input
+echo -e "\r"
+
+# Register with subscription-manager
+subscription-manager register --force --user ${USER} --password ${PASSWD} &> /dev/null
+
+# Refresh subscriptions
+subscription-manager refresh &> /dev/null
+
+# Find the desired Pool ID for OpenShift
+POOL_ID=$(subscription-manager list --available --matches '*OpenShift Container Platform*' | grep "Pool ID" | tail -n 1 | awk -F: '{print $2}' | tr -d ' ')
+
+# Attach to the chosen Pool
+subscription-manager attach --pool="$POOL_ID"
+
+# Add an empty line after the task
+echo
+# ====================================================
+
+
+# === Task: Disable and stop firewalld service ===
+PRINT_TASK "[TASK: Disable and stop firewalld service]"
+
+# List of services to handle
+services=("firewalld")
+
+# Loop through each service in the list
+for service in "${services[@]}"; do
+    # Disable the service
+    systemctl disable "$service" &>/dev/null
+    disable_status=$?
+    # Stop the service
+    systemctl stop "$service" &>/dev/null
+    stop_status=$?
+    # Check if both disable and stop commands executed successfully
+    if [ $disable_status -eq 0 ] && [ $stop_status -eq 0 ]; then
+        echo "ok: [$service service stopped and disabled]"
+    else
+        echo "failed: [$service service is not stopped or disabled]"
+    fi
+done
+
+# Add an empty line after the task
+echo
+# ====================================================
+
+
+
+# === Task: Change SELinux security policy ===
+PRINT_TASK "[TASK: Change SELinux security policy]"
+
+# Read the SELinux configuration
+permanent_status=$(grep "^SELINUX=" /etc/selinux/config | cut -d= -f2)
+# Check if the permanent status is Enforcing
+if [[ $permanent_status == "enforcing" ]]; then
+    # Change SELinux to permissive
+    sed -i 's/^SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+    permanent_status="permissive"
+    echo "OK: [selinux permanent security policy changed to $permanent_status]"
+elif [[ $permanent_status =~ ^[Dd]isabled$ ]] || [[ $permanent_status == "permissive" ]]; then
+    echo "OK: [selinux permanent security policy is $permanent_status]"
+else
+    echo "failed: [selinux permanent security policy is $permanent_status (expected permissive or disabled)]"
+fi
+
+
+# Temporarily set SELinux security policy to permissive
+setenforce 0 &>/dev/null
+# Check temporary SELinux security policy
+temporary_status=$(getenforce)
+# Check if temporary SELinux security policy is permissive or disabled
+if [[ $temporary_status == "Permissive" || $temporary_status == "Disabled" ]]; then
+    echo "ok: [selinux temporary security policy is $temporary_status]"
+else
+    echo "failed: [selinux temporary security policy is $temporary_status (expected Permissive or Disabled)]"
+fi
+
+# Add an empty line after the task
+echo
+# ====================================================
+
 
 # === Task: Install infrastructure rpm ===
 PRINT_TASK "[TASK: Install infrastructure rpm]"
