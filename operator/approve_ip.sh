36ffe88c900f2de34a05c43f681af7bcb7b2f045
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-11-29 18:52:12 +0800

    Create approve_ip.sh

---
+++ operator/approve_ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while true; do
+    echo "waiting for install plan"
+    INSTALLPLAN=$(oc get ip --all-namespaces -o=jsonpath='{range .items[?(@.spec.approved==false)]}{.metadata.name} {.metadata.namespace}{"\n"}{end}')
+    if [[ -n "$INSTALLPLAN" ]]; then
+        NAME=$(echo "$INSTALLPLAN" | awk '{print $1}')
+        NAMESPACE=$(echo "$INSTALLPLAN" | awk '{print $2}')
+        oc patch installplan "$NAME" -n "$NAMESPACE" --type merge --patch '{"spec":{"approved":true}}' 
+        break
+    fi
+    sleep 5
+done
