15aced9e33e74396a8e5cca3ee00e285672d4b60
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-11-30 11:22:51 +0800

    Update approve_ip.sh

--- operator/approve_ip.sh
+++ operator/approve_ip.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+#while true; do
+#    echo "waiting for installplan..."
+#    INSTALLPLAN=$(oc get ip --all-namespaces -o=jsonpath='{range .items[?(@.spec.approved==false)]}{.metadata.name} {.metadata.namespace}{"\n"}{end}')
+#    if [[ -n "$INSTALLPLAN" ]]; then
+#        NAME=$(echo "$INSTALLPLAN" | awk '{print $1}')
+#        NAMESPACE=$(echo "$INSTALLPLAN" | awk '{print $2}')
+#        oc patch installplan "$NAME" -n "$NAMESPACE" --type merge --patch '{"spec":{"approved":true}}' 
+#        break
+#    fi
+#    sleep 15
+#done
+
 while true; do
     echo "waiting for installplan..."
-    INSTALLPLAN=$(oc get ip --all-namespaces -o=jsonpath='{range .items[?(@.spec.approved==false)]}{.metadata.name} {.metadata.namespace}{"\n"}{end}')
+    INSTALLPLAN=$(oc get ip --all-namespaces -o=jsonpath='{range .items[?(@.spec.approved==false)]}{.metadata.name} {.metadata.namespace}{"\n"}{end}' | tr -d '\r')
+    
     if [[ -n "$INSTALLPLAN" ]]; then
-        NAME=$(echo "$INSTALLPLAN" | awk '{print $1}')
-        NAMESPACE=$(echo "$INSTALLPLAN" | awk '{print $2}')
-        oc patch installplan "$NAME" -n "$NAMESPACE" --type merge --patch '{"spec":{"approved":true}}' 
+        echo "$INSTALLPLAN" | while read -r NAME NAMESPACE; do
+            if [[ -n "$NAME" && -n "$NAMESPACE" ]]; then
+                echo "Approving InstallPlan: $NAME in namespace: $NAMESPACE"
+                oc patch installplan "$NAME" -n "$NAMESPACE" --type merge --patch '{"spec":{"approved":true}}'
+            else
+                echo "Skipping invalid entry: NAME=$NAME, NAMESPACE=$NAMESPACE"
+            fi
+        done
         break
     fi
     sleep 15
 done
+
+
 
 # for i in {1..2}; do curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash; done
 
