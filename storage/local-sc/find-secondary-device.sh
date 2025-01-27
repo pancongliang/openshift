7c2290aae10a0103176bf71984e6687d2e972bdc
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-01-27 12:30:13 +0800

    Create find-secondary-device.sh

---
+++ storage/local-sc/find-secondary-device.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -uo pipefail
+
+for device in /dev/sd*; do
+  /usr/sbin/blkid "${device}" &> /dev/null
+  if [ $? == 2 ]; then
+    ls -l /dev/disk/by-path/ | awk -v dev="${device##*/}" '$0 ~ dev {print "/dev/disk/by-path/" $9}'
+    exit
+  fi
+done
+echo "Couldn't find secondary block device!" >&2
