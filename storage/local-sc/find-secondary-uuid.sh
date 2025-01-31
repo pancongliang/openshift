d83c6d0b7c03418a796c337e6259c5115eca01d1
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-01-31 15:31:25 +0800

    Create find-secondary-uuid.sh

---
+++ storage/local-sc/find-secondary-uuid.sh
@@ -0,0 +1,31 @@
+cat << EOF > find-secondary-device.sh
+#!/bin/bash
+set -e
+NODE_NAME=\$(hostname) && DEVICE_FOUND=false && COUNTER=\$1
+for device in /dev/\$DEVICE; do
+  if ! blkid "\$device" &>/dev/null; then
+    mkfs.xfs -f "\$device" &>/dev/null
+    UUID=\$(blkid "\$device" -o value -s UUID 2>/dev/null)
+    if [ -n "\$UUID" ]; then
+      DEVICE_PATH="/dev/disk/by-uuid/\$UUID"
+      echo "\$NODE_NAME: \$DEVICE_PATH" && echo "export DEVICE_PATH_\$COUNTER=\$DEVICE_PATH"
+      DEVICE_FOUND=true 
+      COUNTER=\$((COUNTER + 1))
+    fi
+  fi
+done
+
+if [ "\$DEVICE_FOUND" = false ]; then
+  echo "\$NODE_NAME: - Couldn't find secondary block device!" >&2
+fi
+EOF
+
+
+NODES=$(oc get nodes -l 'node-role.kubernetes.io/worker' -o=jsonpath='{.items[*].metadata.name}')
+
+COUNTER=1
+
+for node in $NODES; do
+  ssh core@$node "sudo bash -s $COUNTER" < find-secondary-device.sh
+  COUNTER=$(($COUNTER + 1))
+done
