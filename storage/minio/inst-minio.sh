47a1c1c44abb3c0d078769519d13c3f0976c7fe8
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-15 22:15:03 +0800

    Update inst-minio.sh

--- storage/minio/inst-minio.sh
+++ storage/minio/inst-minio.sh
@@ -6,10 +6,9 @@ set -o pipefail
 trap 'echo "failed: [line $LINENO: command \`$BASH_COMMAND\`]"; exit 1' ERR
 
 # Set environment variables
-export NAMESPACE="minio"
-#export STORAGE_CLASS_NAME="gp2-csi"
-export STORAGE_CLASS_NAME="managed-nfs-storage"
-export STORAGE_SIZE="50Gi"
+export STORAGE_SIZE="50Gi"   # Requires default storage class
+
+
 
 # Function to print a task with uniform length
 PRINT_TASK() {
@@ -35,27 +34,26 @@ run_command() {
 # ====================================================
 
 
-oc delete ns $NAMESPACE >/dev/null 2>&1 || true
-
 # Print task title
-PRINT_TASK "[TASK: Deploying Minio Object Storage]"
+PRINT_TASK "TASK [Deploying Minio Object Storage]"
 
 # Deploy Minio with the specified YAML template
+oc delete ns minio >/dev/null 2>&1 || true
 sudo curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/storage/minio/deploy-minio-with-persistent-volume.yaml | envsubst | oc apply -f - >/dev/null 2>&1
-run_command "[Applied Minio object]"
+run_command "[deploying minio object storage]]"
 
 # Wait for Minio pods to be in 'Running' state
 # Initialize progress_started as false
 progress_started=false
 while true; do
     # Get the status of all pods
-    output=$(oc get po -n "$NAMESPACE" --no-headers | awk '{print $2, $3}')
+    output=$(oc get po -n minio --no-headers | awk '{print $2, $3}')
     
     # Check if any pod is not in the "1/1 Running" state
     if echo "$output" | grep -vq "1/1 Running"; then
         # Print the info message only once
         if ! $progress_started; then
-            echo -n "info: [Waiting for pods to be in 'running' state"
+            echo -n "info: [waiting for pods to be in 'running' state"
             progress_started=true  # Set to true to prevent duplicate messages
         fi
         
@@ -63,30 +61,31 @@ while true; do
         echo -n '.'
         sleep 2
     else
-        # Close the progress indicator and print the success message
-        echo "]"
-        echo "ok: [Minio pods are in 'running' state]"
+        # Close progress indicator only if progress_started is true
+        if $progress_started; then
+            echo "]"
+        fi
+        echo "ok: [minio pods are in 'running' state]"
         break
     fi
 done
 
+sleep 10
 
 # Get Minio route URL
-export BUCKET_HOST=$(oc get route minio -n ${NAMESPACE} -o jsonpath='http://{.spec.host}')
-run_command "[Retrieved Minio route host: $BUCKET_HOST]"
-
-sleep 20
+export BUCKET_HOST=$(oc get route minio -n minio -o jsonpath='http://{.spec.host}')
+run_command "[retrieved minio host: $BUCKET_HOST]"
 
 # Set Minio client alias
-oc rsh -n ${NAMESPACE} deployments/minio mc alias set my-minio ${BUCKET_HOST} minioadmin minioadmin > /dev/null
-run_command "[Configured Minio client alias]"
+oc rsh -n minio deployments/minio mc alias set my-minio ${BUCKET_HOST} minioadmin minioadmin > /dev/null
+run_command "[configured minio client alias]"
 
 # Create buckets for Loki, Quay, OADP, and MTC
 for BUCKET_NAME in "loki-bucket" "quay-bucket" "oadp-bucket" "mtc-bucket"; do
-    oc rsh -n ${NAMESPACE} deployments/minio mc --no-color mb my-minio/$BUCKET_NAME > /dev/null
-    run_command "[Created bucket $BUCKET_NAME]"
+    oc rsh -n minio deployments/minio mc --no-color mb my-minio/$BUCKET_NAME > /dev/null
+    run_command "[created bucket $BUCKET_NAME]"
 done
 
 # Print Minio address and credentials
-echo "info: [Minio address: $BUCKET_HOST]"
-echo "info: [Minio default ID/PW: minioadmin/minioadmin]"
+echo "info: [minio address: $BUCKET_HOST]"
+echo "info: [minio default id/pw: minioadmin/minioadmin]"
