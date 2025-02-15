7f8b84dfa00d917478850067a311d6fc5bd3cb23
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-15 21:01:53 +0800

    Update inst-mirror-registry.sh

--- registry/mirror-registry/inst-mirror-registry.sh
+++ registry/mirror-registry/inst-mirror-registry.sh
@@ -44,11 +44,11 @@ packages=("wget" "podman")
 package_list="${packages[*]}"
 
 # Install all packages at once
-sudo dnf install -y $package_list &>/dev/null
+sudo dnf install -y $package_list >/dev/null 2>&1
 
 # Check if each package was installed successfully
 for package in "${packages[@]}"; do
-    rpm -q $package &>/dev/null
+    rpm -q $package >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "ok: [installed $package package]"
     else
@@ -66,9 +66,9 @@ echo
 PRINT_TASK "[TASK: Delete existing duplicate data]"
 
 # Check if there is an active mirror registry pod
-if sudo podman pod ps | grep -E 'quay-pod.*Running' >/dev/null; then
+if sudo podman pod ps | grep -E 'quay-pod.*Running' >/dev/null 2>&1; then
     # If the mirror registry pod is running, uninstall it
-    ${REGISTRY_INSTALL_PATH}/mirror-registry uninstall --autoApprove --quayRoot ${REGISTRY_INSTALL_PATH} &>/dev/null
+    ${REGISTRY_INSTALL_PATH}/mirror-registry uninstall --autoApprove --quayRoot ${REGISTRY_INSTALL_PATH} >/dev/null 2>&1
     # Check the exit status of the uninstall command
     if [ $? -eq 0 ]; then
         echo "ok: [uninstall the mirror registry]"
@@ -87,7 +87,7 @@ files=(
 
 for file in "${files[@]}"; do
     if [ -e "$file" ]; then
-        sudo rm -rf "$file" &>/dev/null
+        sudo rm -rf "$file" >/dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo "ok: [delete existing duplicate data: $file]"
         else
@@ -115,12 +115,12 @@ sudo chmod -R 777 ${REGISTRY_INSTALL_PATH}
 run_command "[create ${REGISTRY_INSTALL_PATH} directory]"
 
 # Download mirror-registry
-# wget -P ${REGISTRY_INSTALL_PATH} https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/latest/mirror-registry.tar.gz &> /dev/null
-sudo wget -O ${REGISTRY_INSTALL_PATH}/mirror-registry.tar.gz https://mirror.openshift.com/pub/cgw/mirror-registry/latest/mirror-registry-amd64.tar.gz &> /dev/null
+# wget -P ${REGISTRY_INSTALL_PATH} https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/latest/mirror-registry.tar.gz >/dev/null 2>&1
+sudo wget -O ${REGISTRY_INSTALL_PATH}/mirror-registry.tar.gz https://mirror.openshift.com/pub/cgw/mirror-registry/latest/mirror-registry-amd64.tar.gz >/dev/null 2>&1
 run_command "[download mirror-registry package]"
 
 # Extract the downloaded mirror-registry package
-sudo tar xvf ${REGISTRY_INSTALL_PATH}/mirror-registry.tar.gz -C ${REGISTRY_INSTALL_PATH}/ &> /dev/null
+sudo tar xvf ${REGISTRY_INSTALL_PATH}/mirror-registry.tar.gz -C ${REGISTRY_INSTALL_PATH}/ >/dev/null 2>&1
 run_command "[extract the mirror-registry package]"
 
 echo "ok: [start installing mirror-registry...]"
@@ -173,11 +173,11 @@ sudo update-ca-trust
 run_command "[trust the rootCA certificate]"
 
 # Delete the tar package generated during installation
-sudo rm -rf pause.tar postgres.tar quay.tar redis.tar &>/dev/null
+sudo rm -rf pause.tar postgres.tar quay.tar redis.tar >/dev/null 2>&1
 run_command "[delete the tar package: pause.tar postgres.tar quay.tar redis.tar]"
 
 # loggin registry
-sudo podman login -u ${REGISTRY_ID} -p ${REGISTRY_PW} https://${REGISTRY_DOMAIN_NAME}:8443 &>/dev/null
+sudo podman login -u ${REGISTRY_ID} -p ${REGISTRY_PW} https://${REGISTRY_DOMAIN_NAME}:8443 >/dev/null 2>&1
 run_command  "[login registry https://${REGISTRY_DOMAIN_NAME}:8443]"
 
 # Add an empty line after the task
@@ -193,18 +193,18 @@ REGISTRY_CAS=$(oc get image.config.openshift.io/cluster -o yaml | grep -o 'regis
 if [[ -n "$REGISTRY_CAS" ]]; then
   # If it exists, execute the following commands
   oc delete configmap registry-config -n openshift-config >/dev/null 2>&1 || true
-  oc create configmap registry-config --from-file=${REGISTRY_DOMAIN_NAME}..8443=/etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.ca.pem -n openshift-config &> /dev/null
+  oc create configmap registry-config --from-file=${REGISTRY_DOMAIN_NAME}..8443=/etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.ca.pem -n openshift-config >/dev/null 2>&1
   run_command  "[create a configmap containing the registry CA certificate: registry-config]"
   
-  oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-config"}}}' --type=merge &> /dev/null
+  oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-config"}}}' --type=merge >/dev/null 2>&1
   run_command  "[trust the registry-config configmap]"
 else
   # If it doesn't exist, execute the following commands
   oc delete configmap registry-cas -n openshift-config >/dev/null 2>&1 || true
-  oc create configmap registry-cas --from-file=${REGISTRY_DOMAIN_NAME}..8443=/etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.ca.pem -n openshift-config &> /dev/null
+  oc create configmap registry-cas --from-file=${REGISTRY_DOMAIN_NAME}..8443=/etc/pki/ca-trust/source/anchors/${REGISTRY_DOMAIN_NAME}.ca.pem -n openshift-config >/dev/null 2>&1
   run_command  "[create a configmap containing the registry CA certificate: registry-cas]"
 
-  oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-cas"}}}' --type=merge &> /dev/null
+  oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-cas"}}}' --type=merge >/dev/null 2>&1
   run_command  "[trust the registry-cas configmap]"
 fi
 
@@ -215,14 +215,14 @@ echo
 # Task: Update the global pull-secret
 PRINT_TASK "[TASK: Update the global pull-secret]"
 
-rm -rf pull-secret &>/dev/null
-oc get secret/pull-secret -n openshift-config --output="jsonpath={.data.\.dockerconfigjson}" | base64 -d > pull-secret &>/dev/null
+rm -rf pull-secret >/dev/null 2>&1
+oc get secret/pull-secret -n openshift-config --output="jsonpath={.data.\.dockerconfigjson}" | base64 -d > pull-secret >/dev/null 2>&1
 run_command  "[export pull-secret file]"
 
-podman login --authfile pull-secret ${REGISTRY_DOMAIN_NAME}:8443 &>/dev/null
+podman login --authfile pull-secret ${REGISTRY_DOMAIN_NAME}:8443 >/dev/null 2>&1
 run_command  "[authentication identity information to the pull-secret file]"
 
-oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=pull-secret &>/dev/null
+oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=pull-secret >/dev/null 2>&1
 run_command  "[update pull-secret for the cluster]"
 
 
