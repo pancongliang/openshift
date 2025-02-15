063ae78416f0661848d25d3d3c4180e5e2006d47
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-15 23:32:28 +0800

    Update inst-logging.sh

--- operator/logging/lokistack/inst-logging.sh
+++ operator/logging/lokistack/inst-logging.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+# Enable strict mode for robust error handling and log failures with line number.
+set -u
+set -e
+set -o pipefail
+trap 'echo "failed: [line $LINENO: command \`$BASH_COMMAND\`]"; exit 1' ERR
+
 # Set environment variables
 export CHANNEL_NAME="stable-6.1"
-export STORAGE_CLASS_NAME="gp2-csi"
+export STORAGE_CLASS_NAME="managed-nfs-storage"
 export STORAGE_SIZE="50Gi"
-export CATALOG_SOURCE=cs-redhat-operator-index
-#export CATALOG_SOURCE=redhat-operators
-
-#!/bin/bash
+export CATALOG_SOURCE=redhat-operators
 
 # Function to print a task with uniform length
 PRINT_TASK() {
@@ -16,96 +20,77 @@ PRINT_TASK() {
 
     echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
 }
-# ====================================================
 
 # Function to check command success and display appropriate message
 run_command() {
-    if [ $? -eq 0 ]; then
+    local exit_code=$?
+    if [ $exit_code -eq 0 ]; then
         echo "ok: $1"
     else
         echo "failed: $1"
+        exit 1
     fi
 }
-# ====================================================
 
-# Print task title
-PRINT_TASK "[TASK: Install Minio Tool]"
-
-# Check if mc is already installed and operational
-if mc --version &> /dev/null; then
-    run_command "[MC tool already installed, skipping installation]"
-else
-    # Download the MC tool
-    curl -OL https://dl.min.io/client/mc/release/linux-amd64/mc &> /dev/null
-    run_command "[Downloaded MC tool]"
-
-    # Remove the old version (if it exists)
-    rm -f /usr/local/bin/mc &> /dev/null
-
-    # Move the new version to /usr/local/bin
-    mv mc /usr/local/bin/ &> /dev/null
-    run_command "[Installed MC tool to /usr/local/bin/]"
-
-    # Set execute permissions for the tool
-    chmod +x /usr/local/bin/mc &> /dev/null
-    run_command "[Set execute permissions for MC tool]"
-
-    # Verify the installation
-    if mc --version &> /dev/null; then
-        run_command "[MC tool installation complete]"
-    else
-        run_command "[Failed to install MC tool, proceeding without it]"
-    fi
-fi
-
-echo 
-# ====================================================
-
-# Print task title
-PRINT_TASK "[TASK: Deploying Minio object]"
+# Step 1:
+PRINT_TASK "TASK [Deploying Minio Object Storage]"
 
 # Deploy Minio with the specified YAML template
-export NAMESPACE="minio"
-
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/storage/minio/deploy-minio-with-persistent-volume.yaml | envsubst | oc apply -f - &> /dev/null
-run_command "[Create Minio object]"
+sudo curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/storage/minio/deploy-minio-with-persistent-volume.yaml | envsubst | oc apply -f - >/dev/null 2>&1
+run_command "[deploying minio object storage]"
 
 # Wait for Minio pods to be in 'Running' state
+progress_started=false
 while true; do
-    # Check the status of pods
-    if oc get pods -n "$NAMESPACE" --no-headers | awk '{print $3}' | grep -v "Running" &> /dev/null; then
-        echo "info: [Waiting for pods to be in 'Running' state...]"
-        sleep 20
+    # Get the status of all pods
+    output=$(oc get po -n minio --no-headers | awk '{print $2, $3}')
+    
+    # Check if any pod is not in the "1/1 Running" state
+    if echo "$output" | grep -vq "1/1 Running"; then
+        # Print the info message only once
+        if ! $progress_started; then
+            echo -n "info: [waiting for pods to be in 'running' state"
+            progress_started=true  # Set to true to prevent duplicate messages
+        fi
+        
+        # Print progress indicator (dots)
+        echo -n '.'
+        sleep 2
     else
-        echo "ok: [Minio pods are in 'Running' state]"
+        if $progress_started; then
+            echo "]"
+        fi
+        echo "ok: [minio pods are in 'running' state]"
         break
     fi
 done
 
 # Get Minio route URL
-export BUCKET_HOST=$(oc get route minio -n ${NAMESPACE} -o jsonpath='{.spec.host}')
-run_command "[Retrieved Minio route host: $BUCKET_HOST]"
+export BUCKET_HOST=$(oc get route minio -n minio -o jsonpath='http://{.spec.host}')
+run_command "[retrieved minio route host: $BUCKET_HOST]"
 
-sleep 3
+sleep 20
 
 # Set Minio client alias
-mc --no-color alias set my-minio http://${BUCKET_HOST} minioadmin minioadmin &> /dev/null
-run_command "[Configured Minio client alias]"
+oc rsh -n minio deployments/minio mc alias set my-minio ${BUCKET_HOST} minioadmin minioadmin >/dev/null 2>&1
+run_command "[configured minio client alias]"
 
 # Create buckets for Loki, Quay, OADP, and MTC
-for BUCKET_NAME in "loki-bucket"; do
-    mc --no-color mb my-minio/$BUCKET_NAME &> /dev/null
-    run_command "[Created bucket $BUCKET_NAME]"
-done
+oc rsh -n minio deployments/minio mc --no-color mb my-minio/loki-bucket >/dev/null 2>&1
+run_command "[created bucket loki-bucket]"
 
-echo 
-# ====================================================
+# Print Minio address and credentials
+echo "info: [minio address: $BUCKET_HOST]"
+echo "info: [minio default id/pw: minioadmin/minioadmin]"
 
-# Print task title
+# Add an empty line after the task
+echo
+
+# Step 2:
 PRINT_TASK "[TASK: Install OpenShift Logging]"
 
 # Create a namespace
-cat << EOF | oc apply -f - &> /dev/null
+cat << EOF | oc apply -f - >/dev/null 2>&1
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -115,9 +100,9 @@ metadata:
   labels:
     openshift.io/cluster-monitoring: "true" 
 EOF
-run_command "[Create a openshift-operators-redhat namespace]"
+run_command "[create a openshift-operators-redhat namespace]"
 
-cat << EOF | oc apply -f - &> /dev/null
+cat << EOF | oc apply -f - >/dev/null 2>&1
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -127,10 +112,10 @@ metadata:
   labels:
     openshift.io/cluster-monitoring: "true"
 EOF
-run_command "[Create a openshift-logging namespace]"
+run_command "[create a openshift-logging namespace]"
 
 # Create a OperatorGroup
-cat << EOF | oc apply -f - &> /dev/null
+cat << EOF | oc apply -f - >/dev/null 2>&1
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
@@ -150,9 +135,9 @@ spec:
   source: $CATALOG_SOURCE
   sourceNamespace: openshift-marketplace
 EOF
-run_command "[Create a loki operator]"
+run_command "[create a loki operator]"
 
-cat << EOF | oc apply -f - &> /dev/null
+cat << EOF | oc apply -f - >/dev/null 2>&1
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
@@ -174,9 +159,9 @@ spec:
   source: $CATALOG_SOURCE
   sourceNamespace: openshift-marketplace
 EOF
-run_command "[Create a cluster-logging operator]"
+run_command "[create a cluster-logging operator]"
 
-cat << EOF | oc apply -f - &> /dev/null
+cat << EOF | oc apply -f - >/dev/null 2>&1
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -189,20 +174,20 @@ spec:
   source: $CATALOG_SOURCE
   sourceNamespace: openshift-marketplace
 EOF
-run_command "[Create a cluster observability operator]"
+run_command "[create a cluster observability operator]"
 
 # Approval IP
 export NAMESPACE="openshift-logging"
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash &> /dev/null
-run_command "[Approve cluster-logging install plan]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash >/dev/null 2>&1
+run_command "[approve cluster-logging install plan]"
 
 export NAMESPACE="openshift-operators-redhat"
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash &> /dev/null
-run_command "[Approve loki-operator install plan]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash >/dev/null 2>&1
+run_command "[approve loki-operator install plan]"
 
 export NAMESPACE="openshift-operators"
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash &> /dev/null
-run_command "[Approve cluster-observability-operator install plan]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash >/dev/null 2>&1
+run_command "[approve cluster-observability-operator install plan]"
 
 sleep 30
 
@@ -211,12 +196,12 @@ export BUCKET_HOST=$(oc get route minio -n minio -o jsonpath='http://{.spec.host
 export ACCESS_KEY_ID="minioadmin"
 export ACCESS_KEY_SECRET="minioadmin"
 export BUCKET_NAME="loki-bucket"
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/02-config.yaml | envsubst | oc create -f - &> /dev/null
-run_command "[Create Object Storage secret credentials]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/02-config.yaml | envsubst | oc create -f - >/dev/null 2>&1
+run_command "[create object storage secret credentials]"
 
 # Create loki stack
-curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/03-loki-stack-v6.yaml | envsubst | oc create -f - &> /dev/null
-run_command "[Create loki stack]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/03-loki-stack-v6.yaml | envsubst | oc create -f - >/dev/null 2>&1
+run_command "[create loki stack instance]"
 
 sleep 30
 
@@ -229,34 +214,34 @@ while true; do
         split($2, ready, "/");
         if (ready[1] != ready[2] || $3 != "'$EXPECTED_STATUS'") print "waiting";
     }' | grep -q "waiting"; then
-        echo "info: [Not all pods have reached the expected status, waiting...]"
+        echo "info: [not all pods have reached the expected status, waiting...]"
         sleep 30
     else
-        echo "ok: [All pods in namespace openshift-logging have reached the expected state]"
+        echo "ok: [all pods in namespace openshift-logging have reached the expected state]"
         break
     fi
 done
 
-oc project openshift-logging &> /dev/null
+oc project openshift-logging >/dev/null 2>&1
 
-oc create sa collector -n openshift-logging &> /dev/null
-run_command "[Create a service account for the collector]"
+oc create sa collector -n openshift-logging >/dev/null 2>&1
+run_command "[create a service account for the collector]"
 
-oc adm policy add-cluster-role-to-user logging-collector-logs-writer -z collector &> /dev/null
-run_command "[Allow the collector’s service account to write data to the LokiStack CR]"
+oc adm policy add-cluster-role-to-user logging-collector-logs-writer -z collector >/dev/null 2>&1
+run_command "[allow the collector’s service account to write data to the LokiStack CR]"
 
-oc adm policy add-cluster-role-to-user collect-application-logs -z collector &> /dev/null
-run_command "[Allow the collector’s service account to collect app logs]"
+oc adm policy add-cluster-role-to-user collect-application-logs -z collector >/dev/null 2>&1
+run_command "[allow the collector’s service account to collect app logs]"
 
-oc adm policy add-cluster-role-to-user collect-audit-logs -z collector &> /dev/null
-run_command "[Allow the collector’s service account to collect audit logs]"
+oc adm policy add-cluster-role-to-user collect-audit-logs -z collector >/dev/null 2>&1
+run_command "[allow the collector’s service account to collect audit logs]"
 
-oc adm policy add-cluster-role-to-user collect-infrastructure-logs -z collector &> /dev/null
-run_command "[Allow the collector’s service account to collect infra logs]"
+oc adm policy add-cluster-role-to-user collect-infrastructure-logs -z collector >/dev/null 2>&1
+run_command "[allow the collector’s service account to collect infra logs]"
 
 # Creating CLF CR and UIPlugin
-oc create -f https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/04-clf-ui.yaml &> /dev/null
-run_command "[Creating CLF CR and UIPlugin]"
+oc create -f https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/lokistack/04-clf-ui.yaml >/dev/null 2>&1
+run_command "[creating CLF CR and UIPlugin]"
 
 sleep 30
 
@@ -269,10 +254,10 @@ while true; do
         split($2, ready, "/");
         if (ready[1] != ready[2] || $3 != "'$EXPECTED_STATUS'") print "waiting";
     }' | grep -q "waiting"; then
-        echo "info: [Not all pods have reached the expected status, waiting...]"
+        echo "info: [not all pods have reached the expected status, waiting...]"
         sleep 30
     else
-        echo "ok: [All pods in namespace openshift-logging have reached the expected state]"
+        echo "ok: [all pods in namespace openshift-logging have reached the expected state]"
         break
     fi
 done
