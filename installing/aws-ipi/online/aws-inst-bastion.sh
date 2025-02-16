104b791558323f6ad96c473e5b98bcdaf8a8d47c
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-16 15:16:18 +0800

    Update aws-inst-bastion.sh

--- installing/aws-ipi/online/aws-inst-bastion.sh
+++ installing/aws-ipi/online/aws-inst-bastion.sh
@@ -44,7 +44,7 @@ cli_pager=
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
-run_command "[Set up AWS credentials]"
+run_command "[set up aws credentials]"
 
 # Add an empty line after the task
 echo
@@ -56,19 +56,19 @@ PRINT_TASK "TASK [Get subnet information]"
 SUBNET_NAME=$(aws --region $REGION ec2 describe-subnets \
     --query "Subnets[?contains(Tags[?Key=='Name'].Value | [0], '$CLUSTER_NAME')].[Tags[?Key=='Name'].Value | [0]] | [0]" \
     --output text)
-run_command "[Get subnet name: $SUBNET_NAME]"
+run_command "[get subnet name: $SUBNET_NAME]"
 
 # Get Cluster ID
 CLUSTER_ID=$(echo $SUBNET_NAME | cut -d'-' -f1,2)
-run_command "[Get Cluster ID: $CLUSTER_ID]"
+run_command "[get cluster id: $CLUSTER_ID]"
 
 # Get availability zone
 AVAILABILITY_ZONE=$(echo $SUBNET_NAME | awk -F'-' '{print $(NF-2)"-"$(NF-1)"-"$NF}')
-run_command "[Get Availability Zone: $AVAILABILITY_ZONE]"
+run_command "[get availability zone: $AVAILABILITY_ZONE]"
 
 # Get Public Subnet ID
 PUBLIC_SUBNET_ID=$(aws --region $REGION ec2 describe-subnets --filters Name=tag:Name,Values=$CLUSTER_ID-subnet-public-$AVAILABILITY_ZONE | jq -r '.Subnets[].SubnetId')
-run_command "[Get Public Subnet ID: $PUBLIC_SUBNET_ID]"
+run_command "[get public subnet id: $PUBLIC_SUBNET_ID]"
 
 # Add an empty line after the task
 echo
@@ -78,32 +78,32 @@ PRINT_TASK "TASK [Create Security Group]"
 
 # Get vpn name and vpc id
 VPC_NAME=$(aws --region "$REGION" ec2 describe-vpcs --filters "Name=tag:Name,Values=$CLUSTER_ID*" --query "Vpcs[].Tags[?Key=='Name'].Value | [0]" --output text)
-run_command "[Get vpc name: $VPC_NAME]"
+run_command "[get vpc name: $VPC_NAME]"
 
 VPC_ID=$(aws --region "$REGION" ec2 describe-vpcs --filters "Name=tag:Name,Values=$VPC_NAME" --query "Vpcs[].VpcId" --output text)
-run_command "[Get vpc id: $VPC_ID]"
+run_command "[get vpc id: $VPC_ID]"
 
 # Create security group and get security group ID
 SECURITY_GROUP_NAME="$CLUSTER_ID-sg"
 SECURITY_GROUP_DESCRIPTION="External SSH and all internal traffic"
 SECURITY_GROUP_ID=$(aws --region $REGION ec2 create-security-group --group-name "$SECURITY_GROUP_NAME" --description "$SECURITY_GROUP_DESCRIPTION" --vpc-id $VPC_ID --output text)
-run_command "[Create security group and get security group ID: $SECURITY_GROUP_ID]"
+run_command "[xreate security group and get security group id: $SECURITY_GROUP_ID]"
 
 # Add tag to security group
 aws --region $REGION ec2 create-tags --resources $SECURITY_GROUP_ID --tags Key=Name,Value=$SECURITY_GROUP_NAME > /dev/null
-run_command "[Add tag to security group: $SECURITY_GROUP_NAME]"
+run_command "[add tag to security group: $SECURITY_GROUP_NAME]"
 
 # Add inbound rule - SSH
 aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0 > /dev/null
-run_command "[Add inbound rule - SSH]"
+run_command "[add inbound rule - SSH]"
 
 # Add inbound rule - All traffic
 aws --region $REGION ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol all --port -1 --cidr 0.0.0.0/0 > /dev/null
-run_command "[Add inbound rule - All traffic]"
+run_command "[add inbound rule - all traffic]"
 
 # Add outbound rule - All traffic
 aws --region $REGION ec2 describe-security-groups --group-ids $SECURITY_GROUP_ID | grep -A 5 "IpPermissionsEgress" > /dev/null
-run_command "[Default existing outbound rule - All traffic]"
+run_command "[default existing outbound rule - all traffic]"
 
 # Add an empty line after the task
 echo
@@ -116,14 +116,14 @@ export KEY_PAIR_NAME="$CLUSTER_ID-bastion-key"
 sudo rm -rf $HOME/.ssh/$KEY_PAIR_NAME.pem > /dev/null
 aws --region $REGION ec2 delete-key-pair --key-name $KEY_PAIR_NAME > /dev/null
 aws --region $REGION ec2 create-key-pair --key-name $KEY_PAIR_NAME --query 'KeyMaterial' --output text > $HOME/.ssh/$KEY_PAIR_NAME.pem
-run_command "[Create and download the key pair file: $HOME/.ssh/$KEY_PAIR_NAME.pem]"
+run_command "[create and download the key pair file: $HOME/.ssh/$KEY_PAIR_NAME.pem]"
 
 # Retrieves the latest RHEL AMI ID that matches the specified name pattern
 AMI_ID=$(aws --region $REGION ec2 describe-images \
     --filters "Name=name,Values=RHEL-9.3.0_HVM-*-x86_64-49-Hourly2-GP3" \
     --query "sort_by(Images, &CreationDate)[-1].ImageId" \
     --output text)
-run_command "[Retrieves the latest RHEL9 AMI ID that matches the specified name pattern: $AMI_ID]"
+run_command "[retrieves the latest RHEL9 ami id that matches the specified name pattern: $AMI_ID]"
 
 # Create bastion ec2 instance
 INSTANCE_NAME="$CLUSTER_ID-bastion"
@@ -139,14 +139,14 @@ INSTANCE_ID=$(aws --region $REGION ec2 run-instances \
     --query "Instances[0].InstanceId" \
     --output text
 )
-run_command "[Create bastion ec2 instance: $INSTANCE_NAME]"
+run_command "[create bastion ec2 instance: $INSTANCE_NAME]"
 
 # Wait for instance to be in running state
 aws --region $REGION ec2 wait instance-status-ok --instance-ids $INSTANCE_ID > /dev/null
-run_command "[Wait for $INSTANCE_ID instance to pass the status check]"
+run_command "[wait for $INSTANCE_ID instance to pass the status check]"
 
 aws --region $REGION ec2 wait instance-running --instance-ids $INSTANCE_ID > /dev/null
-run_command "[Wait for $INSTANCE_ID instance to be in running state]"
+run_command "[wait for $INSTANCE_ID instance to be in running state]"
 
 # Add an empty line after the task
 echo
@@ -156,28 +156,28 @@ PRINT_TASK "TASK [Get access to Bastion Instance information]"
 
 # Modify permissions for the key pair file
 chmod 400 $HOME/.ssh/$KEY_PAIR_NAME.pem > /dev/null
-run_command "[Modify permissions for the key pair file: $HOME/.ssh/$KEY_PAIR_NAME.pem]"
+run_command "[modify permissions for the key pair file: $HOME/.ssh/$KEY_PAIR_NAME.pem]"
 
 # Get the public IP address of the bastion ec2 instance
 INSTANCE_IP=$(aws --region $REGION ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-run_command "[Get the public IP address of the instance: $INSTANCE_IP]"
+run_command "[get the public ip address of the instance: $INSTANCE_IP]"
 
 # Create access bastion machine file in current directory
 sudo rm -rf ./ocp-bastion.sh > /dev/null
 sudo cat << EOF > "./ocp-bastion.sh"
 ssh -o StrictHostKeyChecking=no -i "$HOME/.ssh/$KEY_PAIR_NAME.pem" ec2-user@"$INSTANCE_IP"
 EOF
-run_command "[Create access $INSTANCE_NAME file in current directory]"
+run_command "[create access $INSTANCE_NAME file in current directory]"
 
 # Modify permissions for the key pair file
 sudo chmod 777 ./ocp-bastion.sh > /dev/null
-run_command "[Modify permissions for the $INSTANCE_NAME file]"
+run_command "[modify permissions for the $INSTANCE_NAME file]"
 
 # Dowload ocp login script
 sudo cat << EOF > "./ocp-login.sh"
 oc login -u admin -p redhat https://$CLUSTER_API:6443 --insecure-skip-tls-verify=true
 EOF
-run_command "[Create access $INSTANCE_NAME file in current directory]"
+run_command "[create access $INSTANCE_NAME file in current directory]"
 
 # Dowload mirror-registry script
 sudo wget -q https://raw.githubusercontent.com/pancongliang/openshift/main/registry/mirror-registry/inst-mirror-registry.sh
@@ -188,44 +188,44 @@ export REGISTRY_ID="root"
 export REGISTRY_PW="password"                         # 8 characters or more
 export REGISTRY_INSTALL_PATH="\$HOME/quay-install"
 EOF
-run_command "[Dowload mirror-registry script]"
+run_command "[dowload mirror-registry script]"
 
 sudo cat <<EOF >> inst-registry.sh
 # Task: Configuring additional trust stores for image registry access
 PRINT_TASK "TASK [Configuring additional trust stores for image registry access]"
 
 oc login -u admin -p redhat https://$CLUSTER_API:6443 --insecure-skip-tls-verify=true &> /dev/null    
-run_command "[Login ocp cluster]"
+run_command "[login ocp cluster]"
 
 oc get secret/pull-secret -n openshift-config --output="jsonpath={.data.\.dockerconfigjson}" | base64 -d > \$HOME/pull-secret
-run_command "[Dowload pull-secret]"
+run_command "[dowload pull-secret]"
 
 sudo podman login -u "\$REGISTRY_ID" -p "\$REGISTRY_PW" --authfile "\$HOME/pull-secret" "\${REGISTRY_DOMAIN_NAME}:8443" &>/dev/null
-run_command "[Add authentication information to pull-secret]"
+run_command "[add authentication information to pull-secret]"
 
 oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=\$HOME/pull-secret &> /dev/null    
-run_command "[Update pull-secret]"
+run_command "[update pull-secret]"
 
 # Save the PULL_SECRET file either as $XDG_RUNTIME_DIR/containers/auth.json
 sudo cat \$HOME/pull-secret | jq . > \${XDG_RUNTIME_DIR}/containers/auth.json &> /dev/null    
-run_command "[Save the pull-secret file either as \$XDG_RUNTIME_DIR/containers/auth.json]"
+run_command "[save the pull-secret file either as \$XDG_RUNTIME_DIR/containers/auth.json]"
 
 # Create a configmap containing the CA certificate
 oc create configmap registry-config \
      --from-file=\${REGISTRY_DOMAIN_NAME}..8443=/etc/pki/ca-trust/source/anchors/\${REGISTRY_DOMAIN_NAME}.ca.pem \
      -n openshift-config &> /dev/null
-run_command "[Create a configmap containing the CA certificate]"
+run_command "[create a configmap containing the ca certificate]"
 
 # Additional trusted CA
 oc patch image.config.openshift.io/cluster --patch '{"spec":{"additionalTrustedCA":{"name":"registry-config"}}}' --type=merge &> /dev/null
-run_command "[Additional trusted CA]"
+run_command "[additional trusted ca]"
 
 # Disabling the default OperatorHub sources
 oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]' &> /dev/null
-run_command "[Disabling the default OperatorHub sources]"
+run_command "[disabling the default operatorhub sources]"
 echo
 EOF
-run_command "[Update mirror-registry script]"
+run_command "[update mirror-registry script]"
 
 
 # Dowload ocp tool script
@@ -298,12 +298,12 @@ install_tar_gz() {
         echo "ok: [Download $tool_name tool]"        
         # Extract the downloaded tool
         sudo tar xvf "/usr/local/bin/$(basename "$tool_url")" -C "/usr/local/bin/" &> /dev/null
-        run_command "[Unzip to /usr/local/bin/$tool_name]"
+        run_command "[unzip to /usr/local/bin/$tool_name]"
         # Remove the downloaded .tar.gz file
         sudo rm -rf "/usr/local/bin/openshift-client-linux.tar.gz" > /dev/null 
         sudo rm -rf "oc-mirror.tar.gz" > /dev/null 
     else
-        echo "failed: [Download $tool_name tool]"
+        echo "failed: [download $tool_name tool]"
     fi
 }
 
@@ -312,7 +312,7 @@ install_tar_gz "openshift-client" "https://mirror.openshift.com/pub/openshift-v4
 install_tar_gz "oc-mirror" "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/oc-mirror.tar.gz"
 
 sudo chmod a+x oc-mirror > /dev/null 
-run_command "[Modify oc-mirror tool permissions]"
+run_command "[modify oc-mirror tool permissions]"
  
 sudo echo -e "\nClientAliveInterval 120\nClientAliveCountMax 720" | sudo tee -a /etc/ssh/sshd_config &> /dev/null
 sudo systemctl restart sshd &> /dev/null
@@ -325,11 +325,11 @@ source /etc/bash_completion.d/oc_completio &> /dev/null
 # Add an empty line after the task
 echo
 EOF
-run_command "[Dowload ocp tool script]"
+run_command "[dowload ocp tool script]"
 
 # Copy the installation script to the bastion ec2 instance
 sudo scp -o StrictHostKeyChecking=no -o LogLevel=ERROR -i $HOME/.ssh/$KEY_PAIR_NAME.pem ./inst-registry.sh ./inst-ocp-tool.sh ./ocp-login.sh ec2-user@$INSTANCE_IP:~/ > /dev/null 2> /dev/null
-run_command "[Copy the inst-registry.sh and inst-ocp-tool.sh script to the $INSTANCE_NAME]"
+run_command "[copy the inst-registry.sh and inst-ocp-tool.sh script to the $INSTANCE_NAME]"
 
 sudo rm -rf ./inst-*.sh
 sudo rm -rf ./ocp-login.sh
