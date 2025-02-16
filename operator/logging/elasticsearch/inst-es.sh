f27b04ebf174c56632cd42503944382f6f872639
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-16 11:19:20 +0800

    Create inst-es.sh

---
+++ operator/logging/elasticsearch/inst-es.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+# Enable strict mode for robust error handling and log failures with line number.
+set -u
+set -e
+set -o pipefail
+trap 'echo "failed: [line $LINENO: command \`$BASH_COMMAND\`]"; exit 1' ERR
+
+# Set environment variables
+export COLLECTOR="fluentd"
+#export COLLECTOR="vector"
+export CHANNEL_NAME="stable"
+export STORAGE_CLASS_NAME="managed-nfs-storage"
+export CATALOG_SOURCE_NAME=redhat-operators
+
+# Function to print a task with uniform length
+PRINT_TASK() {
+    max_length=110  # Adjust this to your desired maximum length
+    task_title="$1"
+    title_length=${#task_title}
+    stars=$((max_length - title_length))
+
+    echo "$task_title$(printf '*%.0s' $(seq 1 $stars))"
+}
+
+# Function to check command success and display appropriate message
+run_command() {
+    local exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        echo "ok: $1"
+    else
+        echo "failed: $1"
+        exit 1
+    fi
+}
+
+
+# Step 1:
+PRINT_TASK "TASK [Deploying Minio Object Storage]"
+
+# Uninstall first
+echo "info: [uninstall old rhsso resources...]"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/elasticsearch/02-instance-fluentd.yaml | envsubst | oc delete -f - >/dev/null 2>&1 || true
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/elasticsearch/02-instance-vector.yaml | envsubst | oc delete -f - >/dev/null 2>&1 || true
+
+oc delete sub elasticsearch-operator -n openshift-operators-redhat >/dev/null 2>&1 || true
+oc delete sub cluster-logging -n openshift-operators >/dev/null 2>&1 || true
+
+oc get csv -n openshift-operators-redhat | grep elasticsearch-operator | awk '{print $1}' | xargs -I {} oc delete csv {} -n openshift-operators-redhat >/dev/null 2>&1 || true
+oc get csv -n openshift-logging | grep cluster-logging | awk '{print $1}' | xargs -I {} oc delete csv {} -n openshift-logging >/dev/null 2>&1 || true
+
+oc delete ns openshift-operators-redhat >/dev/null 2>&1 || true
+oc delete ns openshift-logging >/dev/null 2>&1 || true
+
+
+# Step 1:
+PRINT_TASK "[TASK: Install OpenShift Logging]"
+
+# Create a namespace
+cat << EOF | oc apply -f - >/dev/null 2>&1
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-operators-redhat 
+  annotations:
+    openshift.io/node-selector: ""
+  labels:
+    openshift.io/cluster-monitoring: "true" 
+EOF
+run_command "[create a openshift-operators-redhat namespace]"
+
+cat << EOF | oc apply -f - >/dev/null 2>&1
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-logging
+  annotations:
+    openshift.io/node-selector: ""
+  labels:
+    openshift.io/cluster-monitoring: "true"
+EOF
+run_command "[create a openshift-logging namespace]"
+
+# Create a OperatorGroup
+cat << EOF | oc apply -f - >/dev/null 2>&1
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: openshift-operators-redhat
+  namespace: openshift-operators-redhat 
+spec: {}
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: "elasticsearch-operator"
+  namespace: "openshift-operators-redhat" 
+spec:
+  channel: ${CHANNEL_NAME}
+  installPlanApproval: "Manual"
+  source: ${CATALOG_SOURCE_NAME}
+  sourceNamespace: "openshift-marketplace"
+  name: "elasticsearch-operator"
+
+EOF
+run_command "[create a elasticsearch operator]"
+
+cat << EOF | oc apply -f - >/dev/null 2>&1
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: cluster-logging
+  namespace: openshift-logging 
+spec:
+  targetNamespaces:
+  - openshift-logging 
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: cluster-logging
+  namespace: openshift-logging 
+spec:
+  channel: ${CHANNEL_NAME}
+  installPlanApproval: "Manual"
+  name: cluster-logging
+  source: $CATALOG_SOURCE_NAME
+  sourceNamespace: openshift-marketplace
+EOF
+run_command "[create a cluster-logging operator]"
+
+# Approval IP
+export NAMESPACE="openshift-logging"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash >/dev/null 2>&1
+run_command "[approve cluster-logging install plan]"
+
+export NAMESPACE="openshift-operators-redhat"
+curl -s https://raw.githubusercontent.com/pancongliang/openshift/refs/heads/main/operator/approve_ip.sh | bash >/dev/null 2>&1
+run_command "[approve elasticsearch-operator install plan]"
+
+sleep 30
+
+if [ "$COLLECTOR" == "fluentd" ]; then
+  curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/elasticsearch/02-instance-fluentd.yaml | envsubst | oc apply -f -
+elif [ "$COLLECTOR" == "vector" ]; then
+  curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/operator/logging/elasticsearch/02-instance-vector.yaml | envsubst | oc apply -f -
+else
+  echo "Invalid collector type specified"
+fi
+
+sleep 30
+
+# Wait for openshift-logging pods to be in 'Running' state
+progress_started=false
+while true; do
+    # Get the status of all pods
+    output=$(oc get po -n openshift-logging --no-headers |grep -v Completed | awk '{print $3}')
+    
+    # Check if any pod is not in the "Running" state
+    if echo "$output" | grep -vq "Running"; then
+        # Print the info message only once
+        if ! $progress_started; then
+            echo -n "info: [waiting for pods to be in 'running' state"
+            progress_started=true  # Prevent duplicate messages
+        fi
+        
+        # Print progress indicator (dots)
+        echo -n '.'
+        sleep 10
+    else
+        # Close the progress indicator if it was started
+        if $progress_started; then
+            echo "]"
+        fi
+
+        echo "ok: [all openshift-logging pods are in 'running' state]"
+        break
+    fi
+done
