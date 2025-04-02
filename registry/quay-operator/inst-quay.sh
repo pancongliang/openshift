5b69c53c7fc7cb05492b6a1f8ef5bce7fd34e7e8
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-04-02 14:43:54 +0800

    Update inst-quay.sh

--- registry/quay-operator/inst-quay.sh
+++ registry/quay-operator/inst-quay.sh
@@ -49,7 +49,7 @@ oc delete ns minio >/dev/null 2>&1 || true
 sleep 15
 
 # Deploy Minio with the specified YAML template
-sudo curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/storage/minio/deploy-minio-with-persistent-volume.yaml | envsubst | oc apply -f - >/dev/null 2>&1
+sudo curl -s https://raw.githubusercontent.com/pancongliang/openshift/main/storage/minio/minio-persistent.yaml | envsubst | oc apply -f - >/dev/null 2>&1
 run_command "[deploying minio object storage]"
 
 # Wait for Minio pods to be in 'Running' state
