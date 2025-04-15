abae12d0f6eb3f54277684a6918c91aa213652db
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-04-15 10:25:30 +0800

    Create ping_unused_ips.sh

---
+++ operator/metallb/ping_unused_ips.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+start_ip=210
+end_ip=220
+network="10.184.134"
+
+for ip in $(seq $start_ip $end_ip); do
+  ping -c 1 -W 1 $network.$ip &> /dev/null && echo "$network.$ip is up" || echo "$network.$ip is down"
+done
