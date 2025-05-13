b84303ca023d52e14081ec44fc7d8c53fa4ab16f
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-05-13 19:18:05 +0800

    Update 02-pre-inst.sh

--- installing/any-platform/offline/02-pre-inst.sh
+++ installing/any-platform/offline/02-pre-inst.sh
@@ -787,8 +787,8 @@ else
 fi
 
 # Delete existing duplicate data
-rm -rf "/etc/pki/ca-trust/source/anchors/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.pem" >/dev/null 2>&1
-rm -rf "${REGISTRY_INSTALL_PATH}" >/dev/null 2>&1
+rm -rf /etc/pki/ca-trust/source/anchors/${REGISTRY_HOSTNAME}.${BASE_DOMAIN}.ca.pem >/dev/null 2>&1
+rm -rf ${REGISTRY_INSTALL_DIR} >/dev/null 2>&1
 
 # Create installation directory
 mkdir -p ${REGISTRY_INSTALL_DIR}
