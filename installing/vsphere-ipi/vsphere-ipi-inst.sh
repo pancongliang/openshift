62ab807109238a4f4986a3071e9d36ee9e6a0e93
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-06-19 17:37:54 +0800

    Update vsphere-ipi-inst.sh

--- installing/vsphere-ipi/vsphere-ipi-inst.sh
+++ installing/vsphere-ipi/vsphere-ipi-inst.sh
@@ -6,7 +6,7 @@ set -o pipefail
 trap 'echo "failed: [line $LINENO: command \`$BASH_COMMAND\`]"; exit 1' ERR
 
 # Set environment variables
-export OCP_VERSION=4.16.20
+export OCP_VERSION=4.18.10
 export PULL_SECRET_PATH="$HOME/pull-secret"           # https://cloud.redhat.com/openshift/install/metal/installer-provisioned
 export CLUSTER_NAME="copan"
 export BASE_DOMAIN="example.com"
