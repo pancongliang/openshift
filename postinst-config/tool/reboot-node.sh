c99aafe4aa04dddd294c265d3be2683a634ae5b7
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-08-05 14:41:46 +0800

    Create reboot-node.sh

---
+++ postinst-config/tool/reboot-node.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for Hostname in $(oc get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}')
+do
+   echo "reboot node $Hostname"
+   ssh -o StrictHostKeyChecking=no core@$Hostname sudo shutdown -r -t 3 &> /dev/null 
+done
