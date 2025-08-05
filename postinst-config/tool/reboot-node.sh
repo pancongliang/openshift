2ffbe72bdfeebff88a09cef3fb0c1a987f3df626
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-08-05 14:50:51 +0800

    Delete postinst-config/tool/reboot-node.sh

--- postinst-config/tool/reboot-node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for Hostname in $(oc get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}')
-do
-   echo "reboot node $Hostname"
-   ssh -o StrictHostKeyChecking=no core@$Hostname sudo shutdown -r -t 3 &> /dev/null 
-done
