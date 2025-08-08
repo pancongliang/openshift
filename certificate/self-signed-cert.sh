f960e647a8533f9de3a384be81820fefff09ca4c
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-08-08 12:01:22 +0800

    Update self-signed-cert.sh

--- certificate/self-signed-cert.sh
+++ certificate/self-signed-cert.sh
@@ -5,7 +5,7 @@ trap 'echo "failed: [line $LINENO: command \`$BASH_COMMAND\`]"; exit 1' ERR
 
 # Default variable
 export DOMAIN="quay.ocp4.example.com"
-export CERTS_PATH="certs"
+export CERTS_DIR="certs"
 
 # Function to print a task with uniform length
 PRINT_TASK() {
@@ -32,53 +32,53 @@ export CA_CN="Test Workspace Signer"
 export OPENSSL_CNF="/etc/pki/tls/openssl.cnf"
 
 # Generate a directory for creating certificates
-rm -rf ${CERTS_PATH} > /dev/null 2>&1
-mkdir -p ${CERTS_PATH} > /dev/null 2>&1
-run_command "[create cert directory: ${CERTS_PATH}]"
+rm -rf ${CERTS_DIR} > /dev/null 2>&1
+mkdir -p ${CERTS_DIR} > /dev/null 2>&1
+run_command "[create cert directory: ${CERTS_DIR}]"
 
 # Generate the root Certificate Authority (CA) key
-openssl genrsa -out ${CERTS_PATH}/ca.key 4096 > /dev/null 2>&1
-run_command "[generate root CA private key]"
+openssl genrsa -out ${CERTS_DIR}/rootCA.key 4096 > /dev/null 2>&1
+run_command "[Generate root CA private key]"
 
 # Generate the root CA certificate
 openssl req -x509 \
   -new -nodes \
-  -key ${CERTS_PATH}/ca.key \
+  -key ${CERTS_DIR}/rootCA.key \
   -sha256 \
   -days 1024 \
-  -out ${CERTS_PATH}/ca.crt \
+  -out ${CERTS_DIR}/rootCA.pem \
   -subj /CN="${CA_CN}" \
   -reqexts SAN \
   -extensions SAN \
   -config <(cat ${OPENSSL_CNF} \
       <(printf '[SAN]\nbasicConstraints=critical, CA:TRUE\nkeyUsage=keyCertSign, cRLSign, digitalSignature')) > /dev/null 2>&1
-run_command "[generate root CA certificate]"
+run_command "[Generate root CA certificate]"
 
-# Generate the domain key
-openssl genrsa -out ${CERTS_PATH}/domain.key 2048 > /dev/null 2>&1
-run_command "[generate private key for domain]"
+# Generate the SSL key
+openssl genrsa -out ${CERTS_DIR}/ssl.key 2048 > /dev/null 2>&1
+run_command "[Generate private key for SSL]"
 
-# Generate a certificate signing request (CSR) for the domain
+# Generate a certificate signing request (CSR) for the SSL
 openssl req -new -sha256 \
-    -key ${CERTS_PATH}/domain.key \
+    -key ${CERTS_DIR}/ssl.key \
     -subj "/O=Local Red Hat CodeReady Workspaces/CN=${DOMAIN}" \
     -reqexts SAN \
     -config <(cat ${OPENSSL_CNF} \
         <(printf "\n[SAN]\nsubjectAltName=DNS:${DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth")) \
-    -out ${CERTS_PATH}/domain.csr > /dev/null 2>&1
-run_command "[generate domain certificate signing request]"
+    -out ${CERTS_DIR}/ssl.csr > /dev/null 2>&1
+run_command "[Generate SSL certificate signing request]"
 
-# Generate the domain certificate (CRT)
+# Generate the SSL certificate (CRT)
 openssl x509 \
     -req \
     -sha256 \
     -extfile <(printf "subjectAltName=DNS:${DOMAIN}\nbasicConstraints=critical, CA:FALSE\nkeyUsage=digitalSignature, keyEncipherment, keyAgreement, dataEncipherment\nextendedKeyUsage=serverAuth") \
     -days 365 \
-    -in ${CERTS_PATH}/domain.csr \
-    -CA ${CERTS_PATH}/ca.crt \
-    -CAkey ${CERTS_PATH}/ca.key \
-    -CAcreateserial -out ${CERTS_PATH}/domain.crt  > /dev/null 2>&1
-run_command "[generate domain certificate]"
+    -in ${CERTS_DIR}/ssl.csr \
+    -CA ${CERTS_DIR}/rootCA.pem \
+    -CAkey ${CERTS_DIR}/rootCA.key \
+    -CAcreateserial -out ${CERTS_DIR}/ssl.cert  > /dev/null 2>&1
+run_command "[Generate SSL certificate]"
 
 # self-signed-certificates 
 # https://access.redhat.com/documentation/en-us/red_hat_codeready_workspaces/2.1/html/installation_guide/installing-codeready-workspaces-in-tls-mode-with-self-signed-certificates_crw
