8577d944258b036cd40cad9450a705b064bfee58
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-06 13:46:18 +0800

    Update inst-rhbk.sh

--- operator/rhbk/inst-rhbk.sh
+++ operator/rhbk/inst-rhbk.sh
@@ -33,14 +33,16 @@ run_command() {
     fi
 }
 
-
 # Step 0:
 PRINT_TASK "TASK [Uninstall old rhsso resources...]"
 
 # Uninstall first
 echo "info: [Uninstall old rhbk resources...]"
+IDENTITY_NAME=$(oc get identities.user.openshift.io -o jsonpath="{.items[?(@.user.name=='${KEYCLOAK_REALM_USER}')].metadata.name}")
+oc delete identity.user.openshift.io "${IDENTITY_NAME}" >/dev/null 2>&1 || true
 oc delete secret openid-client-secret -n openshift-config >/dev/null 2>&1 || true
 oc delete configmap openid-route-ca -n openshift-config >/dev/null 2>&1 || true
+oc delete kkeycloakrealmimport example-realm-import  -n $NAMESPACE  >/dev/null 2>&1 || true
 oc delete keycloak example-kc -n $NAMESPACE  >/dev/null 2>&1 || true
 oc delete secret example-tls-secret -n $NAMESPACE  >/dev/null 2>&1 || true
 oc delete secret keycloak-db-secret -n $NAMESPACE  >/dev/null 2>&1 || true
