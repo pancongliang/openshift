f6e5cc61226cd1a98c91345828ae2bc8093978a2
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-15 18:32:10 +0800

    Update approve_ip.sh

--- operator/approve_ip.sh
+++ operator/approve_ip.sh
@@ -6,7 +6,7 @@ progress_started=false
 # Poll until the maximum retry count is reached
 while true; do
     # Find unapproved InstallPlans in the specified namespace
-    INSTALLPLAN=$(oc get installplan -n "$NAMESPACE" -o=jsonpath='{.items[?(@.spec.approved==false)].metadata.name}')
+    INSTALLPLAN=$(oc get installplan -n "$OPERATOR_NS" -o=jsonpath='{.items[?(@.spec.approved==false)].metadata.name}')
 
     # If an unapproved InstallPlan is found
     if [[ -n "$INSTALLPLAN" ]]; then
@@ -14,14 +14,14 @@ while true; do
         NAME=$(echo "$INSTALLPLAN" | awk '{print $1}')
 
         # Approve the InstallPlan
-        oc patch installplan "$NAME" -n "$NAMESPACE" --type merge --patch '{"spec":{"approved":true}}' &> /dev/null
+        oc patch installplan "$NAME" -n "$OPERATOR_NS" --type merge --patch '{"spec":{"approved":true}}' &> /dev/null
         
         # Close the progress indicator if it was started
         if [[ "$progress_started" == true ]]; then
             echo "]"
         fi
 
-        echo "ok: [Approved install plan: $NAME in namespace $NAMESPACE]"
+        echo "ok: [Approved install plan: $NAME in namespace $OPERATOR_NS]"
         break
     fi
     
@@ -31,13 +31,13 @@ while true; do
         if [[ "$progress_started" == true ]]; then
             echo "]"
         fi
-        echo "failed: [Max retries reached. no unapproved install plans found in $NAMESPACE]"
+        echo "failed: [Max retries reached. no unapproved install plans found in $OPERATOR_NS]"
         break
     fi
     
     # Print progress indicator every 6 seconds
     if [[ "$progress_started" == false ]]; then
-        echo -n "info: [Waiting for unapproved install plans in namespace $NAMESPACE"
+        echo -n "info: [Waiting for unapproved install plans in namespace $OPERATOR_NS"
         progress_started=true
     fi
 
