50cba7d5a72aba7823cf946251ae7c93503fd19b
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-27 17:20:24 +0800

    Update 01-set-params.sh

--- installing/any-platform/online/01-set-params.sh
+++ installing/any-platform/online/01-set-params.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the OpenShift release version
-export OCP_VERSION="4.16.29"
+export OCP_VERSION="4.16.52"
 
 # Specify required parameters for install-config.yaml
 export PULL_SECRET_FILE="$HOME/ocp-inst/pull-secret"   # https://cloud.redhat.com/openshift/install/metal/installer-provisioned
@@ -28,13 +28,13 @@ export WORKER01_HOSTNAME="worker01"
 export WORKER02_HOSTNAME="worker02"
 export WORKER03_HOSTNAME="worker03"
 export BASTION_IP="10.184.134.30"
-export BOOTSTRAP_IP="10.184.134.54"
-export MASTER01_IP="10.184.134.177"
-export MASTER02_IP="10.184.134.156"
-export MASTER03_IP="10.184.134.154"
-export WORKER01_IP="10.184.134.121"
-export WORKER02_IP="10.184.134.216"
-export WORKER03_IP="10.184.134.134"
+export BOOTSTRAP_IP="10.184.134.79"
+export MASTER01_IP="10.184.134.55"
+export MASTER02_IP="10.184.134.118"
+export MASTER03_IP="10.184.134.229"
+export WORKER01_IP="10.184.134.217"
+export WORKER02_IP="10.184.134.91"
+export WORKER03_IP="10.184.134.50"
 
 
 # More options — no changes required!
