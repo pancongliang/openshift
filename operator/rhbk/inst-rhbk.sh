8d3ec4ec6f35f7d4565226a369e6851d2dcebf23
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-12-09 16:59:25 +0800

    Update inst-rhbk.sh

--- operator/rhbk/inst-rhbk.sh
+++ operator/rhbk/inst-rhbk.sh
@@ -237,7 +237,7 @@ while true; do
     if echo "$output" | grep -vq "1/1 Running"; then
         # Print the info message only once
         if ! $progress_started; then
-            echo -n -e "\e[96mINFO\e[0m Waiting for $pod_name pods to be in 'running' state"
+            echo -n -e "\e[96mINFO\e[0m Waiting for $pod_name pods to be in Running state"
             progress_started=true  # Set to true to prevent duplicate messages
         fi
         
@@ -381,7 +381,7 @@ while true; do
     if echo "$output" | grep -vq "1/1 Running"; then
         # Print the info message only once
         if ! $progress_started; then
-            echo -n -e "\e[96mINFO\e[0m Waiting for $pod_name pods to be in 'running' state"
+            echo -n -e "\e[96mINFO\e[0m Waiting for $pod_name pods to be in Running state"
             progress_started=true  # Set to true to prevent duplicate messages
         fi
         
