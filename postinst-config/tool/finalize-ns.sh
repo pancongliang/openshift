53850d14df2e3a89d2a35fd3231a45867b1c0601
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2026-01-16 16:20:20 +0800

    Update finalize-ns.sh

--- postinst-config/tool/finalize-ns.sh
+++ postinst-config/tool/finalize-ns.sh
@@ -22,8 +22,8 @@ oc get namespace "$NAMESPACE" -o json | jq '.spec = {"finalizers":[]}' > temp.js
 # Send the updated namespace spec to the Kubernetes API to finalize deletion
 curl -k -H "Content-Type: application/json" \
      -X PUT --data-binary @temp.json \
-     "http://127.0.0.1:8001/api/v1/namespaces/${NAMESPACE}/finalize"
+     "http://127.0.0.1:8001/api/v1/namespaces/${NAMESPACE}/finalize" 
 
 # Optional: Kill the background oc proxy process
 pkill -f "oc proxy" || true
-rm -rf temp.json  || true
+rm -rf temp.json  || true 
