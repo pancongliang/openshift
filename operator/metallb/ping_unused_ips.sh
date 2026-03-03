02dc11e97deb3218998bd2172134b5f45563f344
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2026-03-03 22:19:56 +0800

    Delete operator/metallb/ping_unused_ips.sh

--- operator/metallb/ping_unused_ips.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-start_ip=100
-end_ip=220
-network="10.184.134"
-
-for ip in $(seq $start_ip $end_ip); do
-  ping -c 1 -W 1 $network.$ip &> /dev/null && echo "$network.$ip is up" || echo "$network.$ip is down"
-done
